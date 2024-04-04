1dfc5c57691c994c9801ddb150777c9c1881ae9a
Andres Camilo Zuñiga-Gonzalez <ancazugo@hotmail.com>
2024-04-04 13:22:38 +0100

    Final changes to vectorize raster tree height

--- main.py
+++ main.py
@@ -11,29 +11,29 @@ if __name__ == "__main__":
     IMAGERY_FOLDER = DATA_FOLDER / "imagery" 
     MASK_FOLDER = DATA_FOLDER / "geopackages"
     uk_planet_folder = IMAGERY_FOLDER / "UK_planet_height"
-    gisa_folder = IMAGERY_FOLDER / "GISA-10m_v01_2016"
 
     input_rasters = list(uk_planet_folder.glob('*.tif'))
-    uk_planet_tif = uk_planet_folder / "UK_planet_height.tif"
-    uk_planet_vrt = uk_planet_folder / "UK_planet_height.vrt"
-    uk_planet_clipped_vrt = uk_planet_folder / "UK_planet_height_clipped.vrt"
-    uk_built_up_dissolved_mask = DATA_FOLDER / "geopackages" / "BUA_2022_GB_3186007556224938771_dissolved.gpkg"
+    # uk_planet_tif = uk_planet_folder / "UK_planet_height.tif"
+    # uk_planet_vrt = uk_planet_folder / "UK_planet_height.vrt"
+    # uk_planet_clipped_vrt = uk_planet_folder / "UK_planet_height_clipped.vrt"
+    # uk_built_up_dissolved_mask = DATA_FOLDER / "geopackages" / "BUA_2022_GB_3186007556224938771_dissolved.gpkg"
     # merge_rasters(input_rasters, output_raster)
     # create_vrt(input_rasters, uk_planet_folder)
     # clip_by_mask(uk_planet_vrt, uk_built_up_dissolved_mask, uk_planet_clipped_vrt)
 
     # Example usage
-    raster_path = uk_planet_folder / "ps_PSScene4Band_2019_00000_00058_154_265_composite_lshm.tif"
-    # vector_path = MASK_FOLDER / "BUA_2022_GB_3186007556224938771_dissolved.gpk"
-    vector_path = "/Users/ancazugo/Library/CloudStorage/GoogleDrive-acz25@cam.ac.uk/My Drive/PhD Thesis/data/geopackages/BUA_2022_GB_3186007556224938771_dissolved.gpkg"
-    output_path = uk_planet_folder / "clipped_example.tif"
+    mask_path = MASK_FOLDER / "BUA_2022_GB_3186007556224938771_dissolved.gpkg"
     min_val, max_val = 3, 70 # Example value range
+    field_name = 'height'
 
-    # Step 1: Read and mask the raster
-    raster_array, raster_profile = read_and_mask_raster(raster_path, min_val, max_val)
+    for i, raster_path in enumerate(input_rasters):
+        print(i)
+        out_name = raster_path.stem + "_processed"
+            
+        output_path = IMAGERY_FOLDER / "UK_planet_height_dissolved" / f"{out_name}_masked.tif"
+        vector_path = MASK_FOLDER / "UK_planet_height_dissolved" / f"{out_name}_dissolved.gpkg"
+        
+        # clip_and_mask_raster(raster_path, mask_path, output_path, min_val, max_val)
 
-    # Step 2 & 3: Clip the raster with the vector mask and save
-    clip_raster_with_vector(raster_array, raster_profile, vector_path, output_path)
-
-    print("Process completed successfully.")
+        vectorise_raster(output_path, vector_path, field_name)
 
