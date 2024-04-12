1a11c44e9228455987d7519160b89749ac05f367
Andres Camilo Zuñiga-Gonzalez <ancazugo@hotmail.com>
2024-04-12 18:55:25 +0100

    Rename base folders

--- main.py
+++ main.py
@@ -1,39 +1,32 @@
 import os
-from dotenv import load_dotenv
+from scripts.constants import *
 from pathlib import Path
+from tqdm import tqdm
 from scripts.raster_operations import *
+from scripts.vector_operations import *
 
 if __name__ == "__main__":
+    pass
 
-    load_dotenv()  # take environment variables from .env.
+    ## Process the building height data
+    ## Dissolve adjacent polygons per tile in the grid and summarize their statistics
+    # MASK_DIR = DATA_DIR / "vector"
+    # uk_building_height_dir = MASK_DIR / "UK_building_height/gdb/"
 
-    DATA_FOLDER = Path(os.getenv("DATA_FOLDER")) 
-    IMAGERY_FOLDER = DATA_FOLDER / "imagery" 
-    MASK_FOLDER = DATA_FOLDER / "geopackages"
-    uk_planet_folder = IMAGERY_FOLDER / "UK_planet_height"
+    # input_vector_dirs = [dir for dir in uk_building_height_dir.glob('*/') if dir.is_dir]
 
-    input_rasters = list(uk_planet_folder.glob('*.tif'))
-    # uk_planet_tif = uk_planet_folder / "UK_planet_height.tif"
-    # uk_planet_vrt = uk_planet_folder / "UK_planet_height.vrt"
-    # uk_planet_clipped_vrt = uk_planet_folder / "UK_planet_height_clipped.vrt"
-    # uk_built_up_dissolved_mask = DATA_FOLDER / "geopackages" / "BUA_2022_GB_3186007556224938771_dissolved.gpkg"
-    # merge_rasters(input_rasters, output_raster)
-    # create_vrt(input_rasters, uk_planet_folder)
-    # clip_by_mask(uk_planet_vrt, uk_built_up_dissolved_mask, uk_planet_clipped_vrt)
+    # for tile_dir in tqdm(input_vector_dirs, desc='UK'):
+    #     # print(tile_dir.stem)
+    #     tile_ouput_dir_name = MASK_DIR / "UK_building_height_dissolved" / tile_dir.stem
+    #     tile_ouput_dir_name.mkdir(parents=True)
 
-    # Example usage
-    mask_path = MASK_FOLDER / "BUA_2022_GB_3186007556224938771_dissolved.gpkg"
-    min_val, max_val = 3, 70 # Example value range
-    field_name = 'height'
+    #     tiles_dirs = [dir for dir in tile_dir.glob('*/') if dir.is_dir]
 
-    for i, raster_path in enumerate(input_rasters):
-        print(i)
-        out_name = raster_path.stem + "_processed"
-            
-        output_path = IMAGERY_FOLDER / "UK_planet_height_dissolved" / f"{out_name}_masked.tif"
-        vector_path = MASK_FOLDER / "UK_planet_height_dissolved" / f"{out_name}_dissolved.gpkg"
-        
-        # clip_and_mask_raster(raster_path, mask_path, output_path, min_val, max_val)
+    #     for small_tile in tqdm(tiles_dirs, desc='Tile'):
+    #         # print(small_tile.stem)
 
-        vectorise_raster(output_path, vector_path, field_name)
+    #         out_name = tile_ouput_dir_name / f'{small_tile.stem}_dissolved.gpkg'
 
+    #         geodatatable_path = [x for x in small_tile.glob("*9.gdbtable")][0]
+
+    #         dissolve_adjacent(geodatatable_path, out_name)
