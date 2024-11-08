f286a4634cdc9a50679431116bb30815142d9699
Andres Camilo Zuñiga-Gonzalez <ancazugo@hotmail.com>
2024-11-08 12:01:44 +0000

    Changes in plots

--- 3-30-300_plots.R
+++ 3-30-300_plots.R
@@ -6,52 +6,29 @@ library(patchwork)
 
 # EDA - Diabetes ----------------------------------------------------------
 
-diabetes_imd_violin_plot <- model_df |>
-    ggplot(aes(x = as.factor(IMD_Decile), y = diabetes_prev, fill = as.factor(IMD_Decile))) +
-    geom_violin() +
-    geom_smooth() +
-    scale_fill_brewer(palette = 'RdYlBu') +
-    labs(y = 'Diabetes Prevalence (%)', x = 'IMD Decile', fill = NULL) +
-    theme_bw() + theme(legend.position = "none")
+# distribution
+three_thirty_hexplot <- ggplot(imd_distance_df) +
+    geom_hex(aes(log(`distance_pch mean`), log(canopy_cover))) +
+    scale_fill_distiller(palette = 'Greens') +
+    labs(x = 'Distance to closest tree',
+         y = 'Canopy cover', fill = 'Frequency') +
+    theme_bw() + theme(aspect.ratio=1)
 
-diabetes_pch_scatter_plot <- model_df |>
-    ggplot(aes(x = d_pch, y = diabetes_prev, colour = as.factor(IMD_Decile))) +
-    geom_point(alpha = .5) +
-    geom_smooth() +
-    scale_color_brewer(palette = 'RdYlBu') +
-    labs(y = 'Diabetes Prevalence (%)', x = 'Distance to closest tree', color = 'IMD decile') +
-    theme_bw() + theme(legend.position = "none")
+three_three_hundred_hexplot <- ggplot(imd_distance_df) +
+    geom_hex(aes(log(`distance_ogs mean`), log(`distance_pch mean`))) +
+    scale_fill_distiller(palette = 'Greens') +
+    labs(x = 'Distance to closest green space',
+         y = 'Distance to closest tree', fill = 'Frequency') +
+    theme_bw() + theme(aspect.ratio=1)
 
-diabetes_canopy_scatter_plot <- model_df |>
-    ggplot(aes(x = canopy_cover, y = diabetes_prev, colour = as.factor(IMD_Decile))) +
-    geom_point(alpha = .5) +
-    geom_smooth() +
-    scale_color_brewer(palette = 'RdYlBu') +
-    labs(y = 'Diabetes Prevalence (%)', x = 'Canopy Cover', color = 'IMD decile') +
-    theme_bw() + theme(legend.position = "none")
+three_hundred_thirty_hexplot <- ggplot(imd_distance_df) +
+    geom_hex(aes(log(`distance_ogs mean`), log(canopy_cover))) +
+    scale_fill_distiller(palette = 'Greens') +
+    labs(x = 'Distance to closest green space',
+         y = 'Canopy cover', fill = 'Frequency') +
+    theme_bw() + theme(aspect.ratio=1)
 
-diabetes_ogs_scatter_plot <- model_df |>
-    ggplot(aes(x = d_ogs, y = diabetes_prev, colour = as.factor(IMD_Decile))) +
-    geom_point(alpha = .5) +
-    geom_smooth() +
-    scale_color_brewer(palette = 'RdYlBu') +
-    labs(y = 'Diabetes Prevalence (%)', x = 'Distance to closest green space', color = 'IMD decile') +
-    theme_bw() + theme(legend.position = "none")
-
-diabetes_pch_scatter_plot <- model_df |> 
-    ggplot(aes(x = IMDScore, y = diabetes_prev, colour = as.factor(IMD_Decile))) +
-    geom_point() +
-    scale_color_brewer(palette = 'RdYlBu') +
-    geom_smooth() +
-    theme_minimal()
-
-diabetes_pch_scatter_plot <- model_df |> 
-    ggplot(aes(x = EnvScore, y = d_ogs, colour = as.factor(IMD_Decile))) +
-    geom_point() +
-    scale_color_brewer(palette = 'RdYlBu') +
-    geom_smooth() +
-    theme_minimal()
-
+# imd vs 3-30-300
 imd_3_boxplot <- ggplot(imd_distance_df) +
     # geom_histogram(aes(log(`distance_pch mean`)), fill = 'darkgreen', color = 'black') +
     # facet_wrap(~LA_decile, nrow = 1) +
@@ -79,35 +56,50 @@ imd_300_boxplot <- ggplot(imd_distance_df) +
     labs(y = 'Distance to closest green space', x = 'IMD decile') +
     theme_bw() + theme(axis.line.x = NULL, legend.position = "none")
 
-# distribution
-three_thirty_hexplot <- ggplot(imd_distance_df) +
-    geom_hex(aes(log(`distance_pch mean`), log(canopy_cover))) +
-    scale_fill_distiller(palette = 'Greens') +
-    labs(x = 'Distance to closest tree',
-         y = 'Canopy cover', fill = 'Frequency') +
-    theme_bw() + theme(aspect.ratio=1)
+# diabetes vs 3-30-300 vs diabetes
+diabetes_imd_violin_plot <- model_df |>
+    ggplot(aes(x = as.factor(IMD_Decile), y = diabetes_prev, fill = as.factor(IMD_Decile))) +
+    geom_violin() +
+    geom_smooth() +
+    scale_fill_brewer(palette = 'RdYlBu') +
+    labs(y = 'Diabetes Prevalence (%)', x = 'IMD Decile', fill = NULL) +
+    theme_bw() + theme(legend.position = "none")
 
-three_three_hundred_hexplot <- ggplot(imd_distance_df) +
-    geom_hex(aes(log(`distance_ogs mean`), log(`distance_pch mean`))) +
-    scale_fill_distiller(palette = 'Greens') +
-    labs(x = 'Distance to closest green space',
-         y = 'Distance to closest tree', fill = 'Frequency') +
-    theme_bw() + theme(aspect.ratio=1)
+diabetes_pch_scatter_plot <- model_df |>
+    ggplot(aes(x = d_pch, y = diabetes_prev, colour = as.factor(IMD_Decile))) +
+    geom_point(alpha = .5) +
+    geom_smooth() +
+    scale_color_brewer(palette = 'RdYlBu') +
+    labs(y = 'Diabetes Prevalence (%)', x = 'Distance to closest tree', color = 'IMD decile') +
+    theme_bw() + theme(legend.position = "none", aspect.ratio = 1)
 
-three_hundred_thirty_hexplot <- ggplot(imd_distance_df) +
-    geom_hex(aes(log(`distance_ogs mean`), log(canopy_cover))) +
-    scale_fill_distiller(palette = 'Greens') +
-    labs(x = 'Distance to closest green space',
-         y = 'Canopy cover', fill = 'Frequency') +
-    theme_bw() + theme(aspect.ratio=1)
+diabetes_canopy_scatter_plot <- model_df |>
+    ggplot(aes(x = canopy_cover, y = diabetes_prev, colour = as.factor(IMD_Decile))) +
+    geom_point(alpha = .5) +
+    geom_smooth() +
+    scale_color_brewer(palette = 'RdYlBu') +
+    labs(y = 'Diabetes Prevalence (%)', x = 'Canopy Cover', color = 'IMD decile') +
+    theme_bw() + theme(legend.position = "none", aspect.ratio = 1)
 
+diabetes_ogs_scatter_plot <- model_df |>
+    ggplot(aes(x = d_ogs, y = diabetes_prev, colour = as.factor(IMD_Decile))) +
+    geom_point(alpha = .5) +
+    geom_smooth() +
+    scale_color_brewer(palette = 'RdYlBu') +
+    labs(y = 'Diabetes Prevalence (%)', x = 'Distance to closest green space', color = 'IMD decile') +
+    theme_bw() + theme(legend.position = "none", aspect.ratio = 1)
 
-
-diabetes_imd_violin_plot / (diabetes_pch_scatter_plot | diabetes_canopy_scatter_plot | diabetes_ogs_scatter_plot) +
+green_plot <- three_thirty_hexplot + three_three_hundred_hexplot + three_hundred_thirty_hexplot + 
     plot_annotation(tag_levels = 'A')
 
-imd_3_boxplot / imd_30_boxplot / imd_300_boxplot +
+ggsave("images/green_dist.png", green_plot, width = 10, height = 2)
+
+green_imd_plot <- imd_3_boxplot / imd_30_boxplot / imd_300_boxplot +
     plot_annotation(tag_levels = 'A')
 
-three_thirty_hexplot + three_three_hundred_hexplot + three_hundred_thirty_hexplot + 
+ggsave("images/green_imd_dist.png", green_imd_plot, width = 6, height = 10)
+
+green_diabetes_plot <- diabetes_imd_violin_plot / (diabetes_pch_scatter_plot | diabetes_canopy_scatter_plot | diabetes_ogs_scatter_plot) +
     plot_annotation(tag_levels = 'A')
+
+ggsave("images/green_diabetes_dist.png", green_diabetes_plot, width = 9, height = 7)
--- Relative Risk plot.R
+++ Relative Risk plot.R
@@ -1,26 +1,46 @@
 library(leaflet)
 
 map <- model_df
-map$RR <- hypothesis_inla_model$summary.fitted.values[, "mean"]
-map$LL <- hypothesis_inla_model$summary.fitted.values[, "0.025quant"]
-map$UL <- hypothesis_inla_model$summary.fitted.values[, "0.975quant"]
+map$RR_b <- base_inla_model$summary.fitted.values[, "mean"]
+map$LL_b <- base_inla_model$summary.fitted.values[, "0.025quant"]
+map$UL_b <- base_inla_model$summary.fitted.values[, "0.975quant"]
+map$RR_eb <- expanded_base_inla_model$summary.fitted.values[, "mean"]
+map$LL_eb <- expanded_base_inla_model$summary.fitted.values[, "0.025quant"]
+map$UL_eb <- expanded_base_inla_model$summary.fitted.values[, "0.975quant"]
+map$RR_h <- hypothesis_inla_model$summary.fitted.values[, "mean"]
+map$LL_h <- hypothesis_inla_model$summary.fitted.values[, "0.025quant"]
+map$UL_h <- hypothesis_inla_model$summary.fitted.values[, "0.975quant"]
+map$RR_eh <- expanded_hypothesis_inla_model$summary.fitted.values[, "mean"]
+map$LL_eh <- expanded_hypothesis_inla_model$summary.fitted.values[, "0.025quant"]
+map$UL_eh <- expanded_hypothesis_inla_model$summary.fitted.values[, "0.975quant"]
 
 pal <- colorNumeric(palette = "YlOrRd", domain = map$RR)
 
 labels <- sprintf("<strong> %s </strong> <br/>
-  Observed: %s <br/> Expected: %s <br/>
-  3-Visibility: %s <br/> 30-Availability: %s <br/>
-  300-Accessibility: %s <br/> SIR: %s <br/> RR: %s (%s, %s)",
+                  Observed: %s <br/> Expected: %s <br/>
+                  3-Visibility: %s <br/> 30-Availability: %s <br/>
+                  300-Accessibility: %s <br/> IncScore: %s <br/> EmpScore: %s <br/>
+                  EduScore: %s <br/> HDDScore: %s <br/> CriScore: %s <br/>
+                  BHSScore: %s <br/> EnvScore: %s <br/> SIR: %s <br/> 
+                  RR_b: %s (%s, %s) <br/>
+                  RR_eb: %s (%s, %s) <br/>
+                  RR_h: %s (%s, %s) <br/>
+                  RR_eh: %s (%s, %s) <br/>",
                   map$lsoa, map$diabetes_qof, round(map$diabetes_expected, 2),
-                  round(map$d_pch, 2), round(map$canopy_cover, 2), round(map$d_ogs, 2), round(map$diabetes_SIR, 2),
-                  round(map$RR, 2),
-                  round(map$LL, 2), round(map$UL, 2)
+                  round(map$d_pch, 2), round(map$canopy_cover, 2), round(map$d_ogs, 2), 
+                  round(map$IncScore, 2), round(map$EmpScore, 2), round(map$EduScore, 2), 
+                  round(map$HDDScore, 2), round(map$CriScore, 2), round(map$BHSScore, 2), 
+                  round(map$EnvScore, 2), round(map$diabetes_SIR, 2),
+                  round(map$RR_b, 2), round(map$LL_b, 2), round(map$UL_b, 2),
+                  round(map$RR_eb, 2), round(map$LL_eb, 2), round(map$UL_eb, 2),
+                  round(map$RR_h, 2), round(map$LL_h, 2), round(map$UL_h, 2),
+                  round(map$RR_eh, 2), round(map$LL_eh, 2), round(map$UL_eh, 2)
 ) %>% lapply(htmltools::HTML)
 
 lRR <- leaflet(map) %>%
     addTiles() %>%
     addPolygons(
-        color = "grey", weight = 1, fillColor = ~ pal(RR),
+        color = "grey", weight = 1, fillColor = ~ pal(RR_eh),
         fillOpacity = 0.5,
         highlightOptions = highlightOptions(weight = 4),
         label = labels,
@@ -34,10 +54,37 @@ lRR <- leaflet(map) %>%
         )
     ) %>%
     addLegend(
-        pal = pal, values = ~RR, opacity = 0.5, title = "RR",
+        pal = pal, values = ~RR_eh, opacity = 0.5, title = "RR",
         position = "bottomright"
     )
 lRR
+
+map <- tibble(.rows = 1)
+map$lsoa <- 1
+map$RR_b <- mean(base_inla_model$summary.fitted.values[, "mean"])
+map$LL_b <- mean(base_inla_model$summary.fitted.values[, "0.025quant"])
+map$UL_b <- mean(base_inla_model$summary.fitted.values[, "0.975quant"])
+map$RR_eb <- mean(expanded_base_inla_model$summary.fitted.values[, "mean"])
+map$LL_eb <- mean(expanded_base_inla_model$summary.fitted.values[, "0.025quant"])
+map$UL_eb <- mean(expanded_base_inla_model$summary.fitted.values[, "0.975quant"])
+map$RR_h <- mean(hypothesis_inla_model$summary.fitted.values[, "mean"])
+map$LL_h <- mean(hypothesis_inla_model$summary.fitted.values[, "0.025quant"])
+map$UL_h <- mean(hypothesis_inla_model$summary.fitted.values[, "0.975quant"])
+map$RR_eh <- mean(expanded_hypothesis_inla_model$summary.fitted.values[, "mean"])
+map$LL_eh <- mean(expanded_hypothesis_inla_model$summary.fitted.values[, "0.025quant"])
+map$UL_eh <- mean(expanded_hypothesis_inla_model$summary.fitted.values[, "0.975quant"])
+
+map |> select(lsoa, ends_with(c('_b', '_eb', '_h', '_eh'))) |> 
+    pivot_longer(RR_b:UL_eh, names_to = 'Risk_dist', values_to = 'Risk_value') |> 
+    mutate(Hypothesis = case_when(Risk_dist |> str_detect('_b') ~ 'Base',
+                                  Risk_dist |> str_detect('_eb') ~ 'Expanded Base',
+                                  Risk_dist |> str_detect('_h') ~ 'Hypothesis',
+                                  Risk_dist |> str_detect('_eh') ~ 'Expanded Hypothesis'),
+           Risk_dist = str_replace(Risk_dist, '_[a-z]*', '')) |> 
+ggplot() +
+    geom_point(aes(x = Risk_value, y = Hypothesis)) +
+    theme_bw()
+
 
 # Critically analysing the 3/30/300 for health
 # hotspot analysis  econometrics (geoda)
