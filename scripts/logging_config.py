e8f546da7781fe5616ff5a656a7601b46e8995bd
Andrés Camilo Zúñiga-González <ancazugo@hotmail.com>
2024-12-03 11:01:19 +0000

    Add docstring to functions

--- scripts/logging_config.py
+++ scripts/logging_config.py
@@ -3,6 +3,14 @@ import logging
 from pathlib import Path
 
 def setup_logger(log_path: str|Path, log_level: str = logging.WARNING) -> None:
+    """
+    Sets up the logging configuration for the application.
+    Parameters:
+        log_path (str | Path): The file path where the log file will be saved.
+        log_level (str): The logging level to be used. Default is logging.WARNING.
+    Returns:
+        None
+    """
     
     logging.basicConfig(filename=log_path,
                         encoding="utf-8",
