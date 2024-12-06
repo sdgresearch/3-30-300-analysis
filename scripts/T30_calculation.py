7dab32b076f3e05c1fd08584704ffa727f817646
Andrés Camilo Zúñiga-González <ancazugo@hotmail.com>
2024-12-06 13:44:01 +0000

    Add docstring

--- scripts/T30_calculation.py
+++ scripts/T30_calculation.py
@@ -9,25 +9,61 @@ from logging_config import *
 import pandas as pd
 import geopandas as gpd
 import numpy as np
+import xarray as xr
 import rioxarray as rxr
 from rioxarray.merge import merge_arrays
 import rasterio as rio
 from rasterstats import zonal_stats
 from tqdm import tqdm
 
-def classify_vom_type(file_name):
+def classify_vom_type(file_name: str|Path) -> str:
+    """
+    Classifies the type of VOM (Volatile Organic Matter) based on the file name.
+    Parameters:
+        file_name (str | Path): The name of the file to classify.
+    Returns:
+        str: 'HS' if the file name contains 'VOM_HS_', otherwise 'CHM'.
+    """
+
     if 'VOM_HS_' in file_name:
         return 'HS'
     else:
         return 'CHM'
     
-def extract_grid_reference(filename):
+def extract_grid_reference(filename: str|Path) -> str|None:
+    """
+    Extracts a grid reference from a given filename.
+    The function searches for a pattern in the filename that matches 'VOM' or 'VOM_HS'
+    followed by an underscore, a two-letter code, a four-digit number, and another underscore.
+    If such a pattern is found, it returns the grid reference (the two-letter code and the four-digit number).
+    If no match is found, it returns None.
+    Parameters:
+        filename (str | Path): The name of the file from which to extract the grid reference.
+    Returns:
+        str | None: The extracted grid reference if a match is found, otherwise None.
+    """
+
     match = re.search(r'VOM(?:_HS)?_([A-Z]{2}\d{4})_', filename)
     if match:
         return match.group(1)
     return None
 
 def translate_tile_name(tile_name: str) -> str:
+    """
+    Translates a tile name between two formats:
+    - Format 1: TL0045 (where '0045' represents coordinates)
+    - Format 2: TL04NW (where 'NW' represents directions)
+    The function converts:
+    - From Format 1 to Format 2 by interpreting the numeric coordinates and converting them to directional codes.
+    - From Format 2 to Format 1 by interpreting the directional codes and converting them to numeric coordinates.
+    Parameters:
+        tile_name (str): The tile name to be translated. It should be a string of length 6.
+    Returns:
+        str: The translated tile name in the opposite format.
+    Raises:
+        AssertionError: If the input tile_name is not of length 6.
+        ValueError: If the numeric part of the tile name cannot be converted to an integer when expected.
+    """
     
     NS_dict = {'S': '0', 'N': '5'}
     EW_dict = {'W': '0', 'E': '5'} 
@@ -51,7 +87,41 @@ def translate_tile_name(tile_name: str) -> str:
 
     return trans_tile_name
 
-def select_chm_files(geo_selected_vom_tiles_df, geo_vom_tiles_df, tif_paths_df,):
+def generate_tiles_paths(geo_level: str, geo_code: str, imd_lsoa_bua_gdf: gpd.GeoDataFrame) -> tuple:
+    """
+    Generates tile paths and associated dataframes for a given geographical level and code.
+    Parameters:
+        geo_level (str): The geographical level (e.g., LSOA, BUA).
+        geo_code (str): The geographical code corresponding to the geo_level.
+        imd_lsoa_bua_gdf (gpd.GeoDataFrame): A GeoDataFrame containing geographical data.
+    Returns:
+        tuple: A tuple containing the following elements:
+            - subgeo_filt_gdf (gpd.GeoDataFrame): A filtered GeoDataFrame for the specified geo_level and geo_code.
+            - geo_boundary_gdf (gpd.GeoDataFrame): A dissolved GeoDataFrame containing the boundary geometry for the specified geo_level.
+            - geo_selected_vom_tiles_df (pd.DataFrame): A DataFrame containing selected VOM tiles for the specified geo_code.
+            - tif_paths_df (pd.DataFrame): A DataFrame containing paths to .tif files, along with their year, tile name, and file type.
+    """
+
+    logging.debug("Generating tile paths")
+
+    subgeo_filt_gdf = imd_lsoa_bua_gdf.copy()[imd_lsoa_bua_gdf[geo_level].isin([geo_code])].reset_index(drop=True)
+
+    geo_vom_tiles_path = vom_lad_dir / f"VOM_tiles_{geo_code}.csv"
+    geo_vom_tiles_df = pd.read_csv(geo_vom_tiles_path).sort_values(['TILE_NAME', 'year'], ascending=[True, False])
+    geo_selected_vom_tiles_df = geo_vom_tiles_df.groupby(['TILE_NAME']).first().reset_index()
+    
+    return subgeo_filt_gdf, geo_vom_tiles_df, geo_selected_vom_tiles_df
+
+def select_chm_files(geo_vom_tiles_df: pd.DataFrame, geo_selected_vom_tiles_df: pd.DataFrame, tif_paths_df: pd.DataFrame) -> list:
+    """
+    Selects CHM (Canopy Height Model) files based on the provided GeoDataFrames and DataFrame of file paths.
+    Parameters:
+        geo_selected_vom_tiles_df (pd.DataFrame): DataFrame containing selected VOM tiles with columns 'TILE_NAME' and 'year'.
+        geo_vom_tiles_df (pd.DataFrame): DataFrame containing VOM tiles with columns 'TILE_NAME' and 'year'.
+        tif_paths_df (pd.DataFrame): DataFrame containing file paths with columns 'TILE_NAME', 'year', 'file_type', and 'path'.
+    Returns:
+        list: A list of Path objects pointing to the selected CHM files.
+    """
 
     logging.debug("Selecting CHM files")
 
@@ -75,27 +145,25 @@ def select_chm_files(geo_selected_vom_tiles_df, geo_vom_tiles_df, tif_paths_df,)
                 else:
                     ind += 1
 
+    # assert len(geo_selected_vom_tiles_df) == len(selected_chm_path_lst)
+
     return selected_chm_path_lst
 
-def generate_tiles_paths(geo_level, geo_code, imd_lsoa_bua_gdf):
+def binarise_tiles(selected_chm_path_lst, low_threshold, high_threshold) -> xr.DataArray:
+    """
+    Binarise the canopy height model (CHM) tiles based on given thresholds.
 
-    logging.debug("Generating tile paths")
-
-    subgeo_filt_gdf = imd_lsoa_bua_gdf.copy()[imd_lsoa_bua_gdf[geo_level].isin([geo_code])].reset_index(drop=True)
-    geo_boundary_gdf = subgeo_filt_gdf.dissolve()[['geometry', geo_level]]
-
-    geo_vom_tiles_path = vom_lad_dir / f"VOM_tiles_{geo_code}.csv"
-    geo_vom_tiles_df = pd.read_csv(geo_vom_tiles_path).sort_values(['TILE_NAME', 'year'], ascending=[True, False])
-    geo_selected_vom_tiles_df = geo_vom_tiles_df.groupby(['TILE_NAME']).first().reset_index()
-
-    tif_paths = list(vom_unzipped_dir.rglob("*.tif"))
-    tif_paths_lst = [[path.parent.name, extract_grid_reference(path.name), classify_vom_type(path.name), str(path)] for path in tif_paths]
-
-    tif_paths_df = pd.DataFrame(tif_paths_lst, columns=['year', 'TILE_NAME', 'file_type', 'path']).sort_values(['TILE_NAME', 'year']).reset_index(drop=True)
-    
-    return subgeo_filt_gdf, geo_boundary_gdf, geo_selected_vom_tiles_df, tif_paths_df
-
-def binarise_tiles(selected_chm_path_lst, low_threshold, high_threshold):
+    This function reads a list of CHM files, merges them into a single xarray DataArray,
+    and then binarises the merged array based on the provided low and high thresholds.
+    The resulting binary array will have values of 1 where the CHM values are within the
+    specified range, and 0 otherwise.
+    Parameters:
+        selected_chm_path_lst (list of str): List of file paths to the CHM tiles to be processed.
+        low_threshold (float): The lower threshold for binarisation.
+        high_threshold (float): The upper threshold for binarisation.
+    Returns:
+        xr.DataArray: A binary xarray DataArray where values are 1 if within the threshold range, and 0 otherwise.
+    """
 
     logging.debug("Binarising tiles")
 
@@ -107,7 +175,15 @@ def binarise_tiles(selected_chm_path_lst, low_threshold, high_threshold):
 
     return binary_merged_chm_xr
 
-def get_canopy_cover(subgeo_filt_gdf, binary_merged_chm_xr):
+def get_canopy_cover(subgeo_filt_gdf: gpd.GeoDataFrame, binary_merged_chm_xr: xr.DataArray) -> pd.DataFrame:
+    """
+    Calculate the canopy cover percentage for each geometry in the given GeoDataFrame.
+    Parameters:
+        subgeo_filt_gdf (gpd.GeoDataFrame): A GeoDataFrame containing the geometries for which the canopy cover is to be calculated.
+        binary_merged_chm_xr (xr.DataArray): A DataArray containing binary canopy height model data.
+    Returns:
+        pd.DataFrame: A DataFrame containing the original geometries and their corresponding canopy cover percentages.
+    """
 
     logging.debug("Calculating canopy cover")
 
@@ -122,7 +198,20 @@ def get_canopy_cover(subgeo_filt_gdf, binary_merged_chm_xr):
     
     return subgeo_canopy_cover_df
 
-def process_geo_code(geo_level, geo_code, imd_lsoa_bua_gdf, low_threshold=3, high_threshold=60):
+def process_geo_code(geo_level: str, geo_code: str, imd_lsoa_bua_gdf: gpd.GeoDataFrame, low_threshold: int=3, high_threshold: int=60) -> None:
+    """
+    Processes geographical data for a given geographical code and level, and calculates the canopy cover.
+
+    Parameters:
+        geo_level (str): The geographical level (e.g., LSOA, BUA).
+        geo_code (str): The geographical code corresponding to the geo_level.
+        imd_lsoa_bua_gdf (gpd.GeoDataFrame): A GeoDataFrame containing geographical data.
+        low_threshold (int, optional): The lower threshold for binarizing canopy height model tiles. Default is 3.
+        high_threshold (int, optional): The upper threshold for binarizing canopy height model tiles. Default is 60.
+
+    Returns:
+        None
+    """
 
     start_time = time.time()
 
@@ -132,8 +221,8 @@ def process_geo_code(geo_level, geo_code, imd_lsoa_bua_gdf, low_threshold=3, hig
 
     logging.info(f"Processing data for {geo_code}")
 
-    subgeo_filt_gdf, geo_boundary_gdf, geo_selected_vom_tiles_df, tif_paths_df = generate_tiles_paths(geo_level, geo_code, imd_lsoa_bua_gdf)
-    selected_chm_path_lst = select_chm_files(geo_selected_vom_tiles_df, geo_selected_vom_tiles_df, tif_paths_df)
+    subgeo_filt_gdf, geo_vom_tiles_df, geo_selected_vom_tiles_df = generate_tiles_paths(geo_level, geo_code, imd_lsoa_bua_gdf)
+    selected_chm_path_lst = select_chm_files(geo_vom_tiles_df, geo_selected_vom_tiles_df, tif_paths_df)
     binary_merged_chm_xr = binarise_tiles(selected_chm_path_lst, low_threshold, high_threshold)
     subgeo_canopy_cover_df = get_canopy_cover(subgeo_filt_gdf, binary_merged_chm_xr)
 
@@ -175,6 +264,11 @@ if __name__ == "__main__":
 
     imd_lsoa_bua_gdf = gpd.read_file(imd_lsoa_bua_boundaries_path).sort_values(by=geo_level)
     geo_level_codes = imd_lsoa_bua_gdf[geo_level].unique()
+
+    tif_paths = list(vom_unzipped_dir.rglob("*.tif"))
+    tif_paths_lst = [[path.parent.name, extract_grid_reference(path.name), classify_vom_type(path.name), str(path)] for path in tif_paths]
+
+    tif_paths_df = pd.DataFrame(tif_paths_lst, columns=['year', 'TILE_NAME', 'file_type', 'path']).sort_values(['TILE_NAME', 'year']).reset_index(drop=True)
 
     if parallel:
         logging.debug("Running in parallel")
