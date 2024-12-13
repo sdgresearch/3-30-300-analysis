8d71b236d4aa89d8fcad36d7be6e8c118a923c3b
Andrés Camilo Zúñiga-González <ancazugo@hotmail.com>
2024-12-13 16:53:37 +0000

    Clean script and correct geojson writing

--- scripts/3-30-300_processing.py
+++ scripts/3-30-300_processing.py
@@ -9,42 +9,7 @@ from sedona_config import *
 
 import geopandas as gpd
 
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description='Description of your script.')
-    parser.add_argument('--log_level', type=str, required=False, default='WARNING', help='Logging level')
-
-    args = parser.parse_args()
-
-    log_level = args.log_level
-
-    project_crs = 'EPSG:27700'
-
-    T3_30_300_DIR = VECTOR_OUT_DIR / "3-30-300"
-    T3_dir = T3_30_300_DIR / "T3"
-    T30_dir = T3_30_300_DIR / "T30"
-    T300_dir = T3_30_300_DIR / "T300"
-    t3_30_300_path = T3_30_300_DIR / "T3_30_300.geojson"
-    imd_lsoa_bua_boundaries_path = VECTOR_OUT_DIR / "IMD" / "English_IMD_2019_BUA_filtered_boundaries.geojson"
-    imd_england_path = VECTOR_IN_DIR / "IMD" / "English IMD 2019" / "IMD_2019.shp"
-    buildings_path = VECTOR_IN_DIR / "EDINA" / "Buildings_6183" / "Buildings_6183.parquet"
-    log_path = Path("logs/T3_calculation_sequential.log")
-
-    setup_logger(log_path=log_path, log_level=log_level)
-    logging.info("Aggregating data for all geographies (3-30-300)")
-    logging.debug("Reading files")
-    start_time = time.time()
-    imd_lsoa_bua_gdf = gpd.read_file(imd_lsoa_bua_boundaries_path)
-    imd_england_columns = ['lsoa11cd', 'TotPop', 'DepChi', 'Pop16_59', 'Pop60+', 'WorkPop',
-                       'IMD_Rank', 'IMD_Decile', 'IMDScore', 'IncScore', 'IncRank', 'IncDec', 
-                       'EmpScore', 'EmpRank', 'EmpDec', 'EduScore', 'EduRank', 'EduDec', 
-                       'HDDScore', 'HDDRank', 'HDDDec', 'CriScore', 'CriRank', 'CriDec', 
-                       'BHSScore', 'BHSRank', 'BHSDec', 'EnvScore', 'EnvRank', 'EnvDec']
-    imd_england_gdf = gpd.read_file(imd_england_path)[imd_england_columns]
-    
-    logging.debug("Setting up Apache Sedona")
-    os.environ["JAVA_HOME"] = JAVA_HOME
-    sedona = get_spark()
+def create_schemas(sedona, imd_lsoa_bua_gdf, imd_england_gdf, buildings_path, T3_dir, T30_dir, T300_dir) -> None:
 
     boundaries_sdf = sedona.createDataFrame(imd_lsoa_bua_gdf.drop(columns=['LSOA21NMW', 'LAD22NMW', 'BUA22NMG', 'BUA22NMW', 'RGN22NMW'], axis=1))
     boundaries_sdf.createOrReplaceTempView('boundaries')
@@ -60,7 +25,8 @@ if __name__ == "__main__":
     t300_sdf = sedona.read.csv(str(T300_dir), header=True, inferSchema=True)
     t300_sdf.createOrReplaceTempView("t300")
 
-    logging.debug("Running queries")
+def run_queries(sedona):
+
     buildings_lsoa_sdf = sedona.sql(
     """
     SELECT b.*, l.LSOA11CD
@@ -104,8 +70,57 @@ if __name__ == "__main__":
     t3_30_300_sdf = t3_30_300_sdf.drop("lsoa11cd")
     t3_30_300_sdf.createOrReplaceTempView("t3_30_300")
 
+    return t3_30_300_sdf
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Description of your script.')
+    parser.add_argument('--log_level', type=str, required=False, default='WARNING', help='Logging level')
+
+    args = parser.parse_args()
+
+    log_level = args.log_level
+
+    project_crs = 'EPSG:27700'
+
+    T3_30_300_DIR = VECTOR_OUT_DIR / "3-30-300"
+    T3_dir = T3_30_300_DIR / "T3"
+    T30_dir = T3_30_300_DIR / "T30"
+    T300_dir = T3_30_300_DIR / "T300"
+    t3_30_300_path = T3_30_300_DIR / "T3_30_300.geojson"
+    imd_lsoa_bua_boundaries_path = VECTOR_OUT_DIR / "IMD" / "English_IMD_2019_BUA_filtered_boundaries.geojson"
+    imd_england_path = VECTOR_IN_DIR / "IMD" / "English IMD 2019" / "IMD_2019.shp"
+    buildings_path = VECTOR_IN_DIR / "EDINA" / "Buildings_6183" / "Buildings_6183.parquet"
+    log_path = Path("logs/T3_calculation_sequential.log")
+
+    setup_logger(log_path=log_path, log_level=log_level)
+    logging.info("Aggregating data for all geographies (3-30-300)")
+    logging.debug("Reading files")
+    start_time = time.time()
+    imd_lsoa_bua_gdf = gpd.read_file(imd_lsoa_bua_boundaries_path)
+    imd_england_columns = ['lsoa11cd', 'TotPop', 'DepChi', 'Pop16_59', 'Pop60+', 'WorkPop',
+                       'IMD_Rank', 'IMD_Decile', 'IMDScore', 'IncScore', 'IncRank', 'IncDec', 
+                       'EmpScore', 'EmpRank', 'EmpDec', 'EduScore', 'EduRank', 'EduDec', 
+                       'HDDScore', 'HDDRank', 'HDDDec', 'CriScore', 'CriRank', 'CriDec', 
+                       'BHSScore', 'BHSRank', 'BHSDec', 'EnvScore', 'EnvRank', 'EnvDec']
+    imd_england_gdf = gpd.read_file(imd_england_path)[imd_england_columns]
+    
+    logging.debug("Setting up Apache Sedona")
+    os.environ["JAVA_HOME"] = JAVA_HOME
+    sedona = get_spark()
+    
+    logging.debug("Running queries")
+    
+    create_schemas(sedona, imd_lsoa_bua_gdf, imd_england_gdf, buildings_path, T3_dir, T30_dir, T300_dir)
+    t3_30_300_sdf = run_queries(sedona)
+
     logging.debug("Saving final output")
-    t3_30_300_sdf.write.mode("overwrite").format("geojson").save(str(t3_30_300_path))
+
+    t3_30_300_df = t3_30_300_sdf.toPandas()
+    t3_30_300_df = t3_30_300_df.replace([float('inf'), float('-inf')], -99)
+    t3_30_300_gdf = t3_30_300_df.set_geometry('geometry')
+    t3_30_300_gdf = t3_30_300_gdf.set_crs(project_crs)
+    t3_30_300_gdf.to_file(t3_30_300_path)
 
     end_time = time.time()
     logging.info(f"Processing 3-30-300 data took {end_time - start_time:.2f} seconds")
