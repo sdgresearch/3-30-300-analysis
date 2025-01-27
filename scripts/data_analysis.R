c904b8a9ceda37f4c20d31a43f57f7b817d4b5ab
Andres Camilo Zuñiga-Gonzalez <ancazugo@hotmail.com>
2025-01-27 00:43:59 +0000

    Add Rank Map

--- scripts/data_analysis.R
+++ scripts/data_analysis.R
@@ -9,17 +9,19 @@ library(ggbump)
 library(GGally)
 library(ggbeeswarm)
 library(ggstatsplot)
+library(BBmisc)
 library(corrplot)
 library(factoextra)
 
 source("scripts/constants.R")
-
 
 # Paths -------------------------------------------------------------------
 
 T3_30_300_DIR <-  here(VECTOR_OUT_DIR, "3-30-300")
 t3_30_300_path <-  here(T3_30_300_DIR, "T3_30_300.geojson")
 
+lsoa_to_bua_path <- here(TABULAR_IN_DIR, "ONS", "LSOA_(2021)_to_Built_Up_Area_to_Local_Authority_District_to_Region_(December_2022)_Lookup_in_England_and_Wales_v2.csv")
+lad_to_bua_path <- here(VECTOR_IN_DIR, "ONS", "Local_Authority_Districts_December_2022_UK_BUC_V2_-1856850221694639751.geojson")
 
 # Variables ---------------------------------------------------------------
 
@@ -123,7 +125,6 @@ t3_30_300_region_boxplots <- t3_region_boxplots / t30_region_boxplots / t300_reg
 ggsave("images/t3_30_300_region_boxplots.png", t3_30_300_region_boxplots, 
        width = 180, height = 170, units = 'mm', dpi = 300)
 
-
 # Scatter Plots -----------------------------------------------------------
 
 t3_30_300_gdf |> 
@@ -192,30 +193,110 @@ t3_30_300_cor |>
 
 # Rank Map ----------------------------------------------------------------
 
+lsoa_to_bua_df <- read_csv(lsoa_to_bua_path)
+lad_to_bua_gdf <- read_sf(lad_to_bua_path)
+
+rgn22_gdf <- lad_to_bua_gdf |> 
+    left_join(lsoa_to_bua_df |> 
+                  select(LAD22CD, RGN22CD, RGN22NM), by = c('LAD22CD' = 'LAD22CD')) |>
+    select(RGN22CD, RGN22NM, geometry) |> 
+    group_by(RGN22CD, RGN22NM) |> 
+    summarise() |> 
+    mutate(RGN22NM = if_else(RGN22NM == 'Wales', NA, RGN22NM)) |>
+    mutate(RGN22NM = fct_relevel(RGN22NM, c('North West', 'North East',
+                                            'Yorkshire and The Humber',
+                                            'West Midlands', 'East Midlands',
+                                            'East of England', 'South West',
+                                            'South East', 'London', NA))) |> 
+    filter(!is.na(RGN22NM))
+
 t3_30_300_rank_gdf <- t3_30_300_gdf |> 
     st_drop_geometry() |> 
-    select(RGN22CD, ends_with('Score'), Total:Pop_density, tree_count, canopy_cover, park_distance,
-           water_distance, NDVI, NDBI, NDWI) |> 
+    select(RGN22CD, ends_with('Score'), park_distance, water_distance,
+           tree_count, canopy_cover, NDVI, NDBI, NDWI, area, Total) |> 
     filter(!is.na(RGN22CD), RGN22CD != 'W92000004') |> 
     group_by(RGN22CD) |> 
     summarise(across(ends_with('Score'), mean, .names = '{.col}'),
-              across(Total:Pop_density, sum, .names = '{.col}'),
-              across(canopy_cover:water_distance, ~ mean(.x, na.rm = TRUE), .names = '{.col}')) |> 
-    mutate(across(IMDScore:park_distance, function(x) {rank(-x)}, .names = '{.col}_rank')) |> 
-    mutate(across(tree_count_rank:canopy_cover_rank, function(x) {n() + 1 - x}, .names = '{.col}')) |>
+              across(area:Total, sum, .names = '{.col}'),
+              across(park_distance:NDWI, ~ mean(.x, na.rm = TRUE), .names = '{.col}')) |>
+    mutate(across(IMDScore:water_distance, function(x) {rank(-x)}, .names = '{.col}_rank')) |> 
+    mutate(across(tree_count:NDWI, function(x) {rank(x)}, .names = '{.col}_rank')) |> 
     mutate(IMD = as.character(IMDScore_rank)) |> 
-    select(RGN22CD, IMD, ends_with('rank')) |> 
-    pivot_longer(ends_with('rank'), names_to = 'Metric', values_to = 'Rank')
-    # select(ends_with('Score'), ends_with('ratio'), area, TotPop_density,  starts_with('3'))
-    # mutate(across(I))
+    select(RGN22CD, IMD, ends_with('rank'))
 
-t3_30_300_rank_gdf |> 
-    filter(Metric %in% c('IMDScore_rank', 'tree_count_rank',
-                         'canopy_cover_rank', 'park_distance_rank')) |> 
-    mutate(Metric = factor(Metric, 
-                           levels = c('IMDScore_rank', 'tree_count_rank',
-                                      'canopy_cover_rank', 'park_distance_rank'),
-                           ordered = T)) |> 
+t3_30_300_rank_long_gdf <- t3_30_300_rank_gdf |> 
+    pivot_longer(ends_with('rank'), names_to = 'Metric', values_to = 'Rank') |> 
+    mutate(Metric = fct_relevel(Metric, c('IMDScore_rank', 'IncScore_rank', 'EmpScore_rank',
+                                          'EduScore_rank', 'HDDScore_rank', 'CriScore_rank',
+                                          'BHSScore_rank', 'EnvScore_rank', 'area_rank',
+                                          'Total_rank', 'tree_count_rank', 'park_distance_rank',
+                                          'canopy_cover_rank', 'water_distance_rank',
+                                          'NDVI_rank', 'NDBI_rank', 'NDWI_rank')))
+
+x_start <- 2
+x_span <- 2
+metric_names <- c('3', '30', '300', 'Water \nDistance', 'NDVI', 'NDWI', 'NDBI', 'IMD')
+text_labels_df <- tibble(metric_names, y = 56.3,
+                         x = seq(x_start, x_start + x_span * (nrow(text_labels_df) - 1), x_span))
+
+t3_30_300_rank_rgn_gdf <- t3_30_300_rank_gdf |> 
+    left_join(st_geometry(rgn22_gdf) |> 
+                  st_point_on_surface() |> 
+                  st_coordinates() |>
+                  as_tibble() |> bind_cols(rgn22_gdf |> select(RGN22CD, RGN22NM)), by = 'RGN22CD') |> 
+    mutate_at(vars(ends_with('rank')), ~ normalize(., range = c(st_bbox(rgn22_gdf)$ymin, st_bbox(rgn22_gdf)$ymax),
+                                                   method = 'range'))
+
+t3_30_300_spectral_rank_map <- t3_30_300_rank_rgn_gdf |> 
+    ggplot() +
+    geom_sf(data = rgn22_gdf, fill = 'transparent') +
+    geom_point(aes(x = X, y = Y, color = IMD)) +
+    
+    geom_sigmoid(aes(x = X, y = Y, xend = x_start + x_span * 0, yend = tree_count_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 0, y = tree_count_rank, color = IMD)) +
+    
+    geom_sigmoid(aes(x = x_start + x_span * 0, y = tree_count_rank, xend = x_start + x_span * 1, yend = canopy_cover_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 1, y = canopy_cover_rank, color = IMD)) +
+    
+    geom_sigmoid(aes(x = x_start + x_span * 1, y = canopy_cover_rank, xend = x_start + x_span * 2, yend = park_distance_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 2, y = park_distance_rank, color = IMD)) +
+    
+    geom_sigmoid(aes(x = x_start + x_span * 2, y = park_distance_rank, xend = x_start + x_span * 3, yend = water_distance_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 3, y = water_distance_rank, color = IMD)) +
+    
+    geom_sigmoid(aes(x = x_start + x_span * 3, y = water_distance_rank, xend = x_start + x_span * 4, yend = NDVI_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 4, y = NDVI_rank, color = IMD)) +
+    
+    geom_sigmoid(aes(x = x_start + x_span * 4, y = NDVI_rank, xend = x_start + x_span * 5, yend = NDWI_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 5, y = NDWI_rank, color = IMD)) +
+    
+    geom_sigmoid(aes(x = x_start + x_span * 5, y = NDWI_rank, xend = x_start + x_span * 6, yend = NDBI_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 6, y = NDBI_rank, color = IMD)) +
+    
+    geom_sigmoid(aes(x = x_start + x_span * 6, y = NDBI_rank, xend = x_start + x_span * 7, yend = IMDScore_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 7, y = IMDScore_rank, color = IMD)) +
+    
+    geom_text(aes(x = x_start + x_span * 7 + .5, y = IMDScore_rank, 
+                  label = str_wrap(RGN22NM, width = 15)), size = 2, hjust = 0) +
+    
+    geom_text(data = text_labels_df, aes(x = x, y = y, label = metric_names), size = 2) +
+    scale_x_continuous(limits = c(-5.5, 18)) +
+    scale_color_brewer(palette = "RdYlBu") +
+    guides(color = guide_legend(nrow = 1)) +
+    theme_void() +
+    theme(plot.background = element_rect(fill = "white", color = 'transparent'),
+          legend.position = "none",
+          legend.title = element_text(size = 20),
+          legend.text = element_text(size = 15))
+
+ggsave("images/t3_30_300_spectral_rank_map.png", t3_30_300_spectral_rank_map, 
+       width = 180, height = 90, units = 'mm', dpi = 300)
+
+
+t3_30_300_rank_long_gdf |> 
+    filter(Metric %in% c('tree_count_rank', 'canopy_cover_rank',
+                         'park_distance_rank', 'water_distance_rank', 
+                         'NDVI_rank', 'NDBI_rank', 'NDWI_rank', 'IMDScore_rank')) |> 
     left_join(t3_30_300_gdf |> 
                   st_drop_geometry() |> 
                   select(RGN22CD, RGN22NM), by = 'RGN22CD') |> 
@@ -229,78 +310,6 @@ ggplot(aes(x = Metric, y = Rank, group = RGN22CD, colour = IMD)) +
     labs(x = 'Metric', y = 'Rank') +
     plot_theme
 
+
 # Low Scors -> High Ranking -> Low Deprivation
 # High Score -> Low Ranking -> High Deprivation
-
-pacman::p_load(BBmisc, tidyverse, hablar, ggbump, sf, rnaturalearth, feather, janitor, lubridate)
-options(stringsAsFactors = F)
-set_wd_to_script_path()
-
-gdpr_violations <- readr::read_tsv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-04-21/gdpr_violations.tsv')
-
-df <- gdpr_violations %>% 
-    group_by(name) %>% 
-    summarise(price = sum_(price)) %>% 
-    ungroup()
-
-sdf <- rnaturalearthdata::countries50 %>% 
-    st_as_sf() %>% 
-    st_crop(xmin = -24, xmax = 31, ymin = 33, ymax = 73) %>% 
-    filter(admin %in% df$name) %>% 
-    left_join(df, by = c("admin" = "name")) %>% 
-    mutate(price_cap  = price / pop_est,
-           admin = case_when(admin == "United Kingdom" ~ "UK",
-                             admin == "Czech Republic" ~ "Czech",
-                             T ~ admin))
-
-ranking <- st_geometry(sdf) %>% 
-    st_point_on_surface() %>% 
-    st_coordinates() %>% 
-    as_tibble() %>% 
-    bind_cols(tibble(fine_cap = normalize(rank(sdf$price_cap), range = c(40.12161, 66.12161), method = "range"),
-                     country = sdf$admin,
-                     xend = 60,
-                     x_axis_start = xend + 10,
-                     fine_cap_x = normalize(sdf$price_cap, range = c(first(x_axis_start), 100), method = "range"),
-                     val_txt = paste0(format(sdf$price_cap, digits = 1, nsmall = 2)),
-                     val_txt2 = if_else(country == "Austria", paste0(val_txt, "€ per capita"), val_txt)
-                     )
-              )
-
-sdf <- sdf %>% 
-    bind_cols(ranking %>% select(fine_cap))
-
-ggplot() + 
-    geom_sf(data = sdf, size = .3, fill = "transparent", color = "gray17") +
-    # Sigmoid from country to start of barchart
-    geom_sigmoid(data = ranking, 
-                 aes(x = X, y = Y, xend = x_axis_start - .2, yend = fine_cap, group = country, color = fine_cap), 
-                 alpha = .6, smooth = 10, size = 1) + 
-    # Line from xstart to value
-    geom_segment(data = ranking, 
-                 aes(x = x_axis_start, y = fine_cap, xend = fine_cap_x, yend = fine_cap, color = fine_cap), alpha = .6, size = 1, 
-                 lineend = "round") + 
-    # Y axis - black line
-    geom_segment(data = ranking, 
-                 aes(x = x_axis_start, y = 40, xend = x_axis_start, yend = 67), alpha = .6, size = 1.3, color = "black") +
-    # dot on centroid of country in map
-    geom_point(data = ranking, 
-               aes(x = X, y = Y, color = fine_cap), size = 2) +
-    # Country text
-    geom_text(data = ranking, aes(x = x_axis_start-.5, y = fine_cap, label = country, color = fine_cap), hjust = 1, size = 2.5, nudge_y = .5) +
-    # Value text
-    geom_text(data = ranking, aes(x = fine_cap_x, y = fine_cap, label = val_txt2, color = fine_cap), hjust = 0, size = 2, nudge_x = .4) +
-    coord_sf(clip = "off") +
-    scale_fill_viridis_c() +
-    scale_color_viridis_c() +
-    theme_void() +
-    labs(title = "GDPR fines per capita",
-         subtitle = str_wrap("The General Data Protection Regulation (EU) 2016/679 (GDPR) is a regulation in EU law on data protection and privacy in the European Union (EU) and the European Economic Area (EEA).", 100),
-         caption = "Source: TidyTuesday & Wikipedia") +
-    theme(plot.margin = margin(.5, 1, .5, .5, "cm"),
-          legend.position = "none",
-          plot.background = element_rect(fill = "black"),
-          plot.caption = element_text(color = "gray40"),
-          plot.title = element_text(color = "gray40", size = 16, family = "Helvetica", face = "bold"),
-          plot.subtitle = element_text(color = "gray40", size = 8))
-
