4079ac82d64f475472d066a5cd44c6ad76bb0c5b
Andres Camilo Zuñiga-Gonzalez <ancazugo@hotmail.com>
2025-01-27 01:25:34 +0000

    Change linetype

--- scripts/data_analysis.R
+++ scripts/data_analysis.R
@@ -125,6 +125,130 @@ t3_30_300_region_boxplots <- t3_region_boxplots / t30_region_boxplots / t300_reg
 ggsave("images/t3_30_300_region_boxplots.png", t3_30_300_region_boxplots, 
        width = 180, height = 170, units = 'mm', dpi = 300)
 
+# Rank Map ----------------------------------------------------------------
+
+lsoa_to_bua_df <- read_csv(lsoa_to_bua_path)
+lad_to_bua_gdf <- read_sf(lad_to_bua_path)
+
+rgn22_gdf <- lad_to_bua_gdf |> 
+    left_join(lsoa_to_bua_df |> 
+                  select(LAD22CD, RGN22CD, RGN22NM), by = c('LAD22CD' = 'LAD22CD')) |>
+    select(RGN22CD, RGN22NM, geometry) |> 
+    group_by(RGN22CD, RGN22NM) |> 
+    summarise() |> 
+    mutate(RGN22NM = if_else(RGN22NM == 'Wales', NA, RGN22NM)) |>
+    mutate(RGN22NM = fct_relevel(RGN22NM, c('North West', 'North East',
+                                            'Yorkshire and The Humber',
+                                            'West Midlands', 'East Midlands',
+                                            'East of England', 'South West',
+                                            'South East', 'London', NA))) |> 
+    filter(!is.na(RGN22NM))
+
+t3_30_300_rank_gdf <- t3_30_300_gdf |> 
+    st_drop_geometry() |> 
+    select(RGN22CD, ends_with('Score'), park_distance, water_distance,
+           tree_count, canopy_cover, NDVI, NDBI, NDWI, area, Total) |> 
+    filter(!is.na(RGN22CD), RGN22CD != 'W92000004') |> 
+    group_by(RGN22CD) |> 
+    summarise(across(ends_with('Score'), mean, .names = '{.col}'),
+              across(area:Total, sum, .names = '{.col}'),
+              across(park_distance:NDWI, ~ mean(.x, na.rm = TRUE), .names = '{.col}')) |>
+    mutate(across(IMDScore:water_distance, function(x) {rank(-x)}, .names = '{.col}_rank')) |> 
+    mutate(across(tree_count:NDWI, function(x) {rank(x)}, .names = '{.col}_rank')) |> 
+    mutate(IMD = as.character(IMDScore_rank)) |> 
+    select(RGN22CD, IMD, ends_with('rank'))
+
+t3_30_300_rank_long_gdf <- t3_30_300_rank_gdf |> 
+    pivot_longer(ends_with('rank'), names_to = 'Metric', values_to = 'Rank') |> 
+    mutate(Metric = fct_relevel(Metric, c('tree_count_rank', 'canopy_cover_rank', 
+                                          'park_distance_rank', 'water_distance_rank',
+                                          'NDVI_rank', 'NDWI_rank', 'NDBI_rank',
+                                          'IMDScore_rank', 'IncScore_rank', 'EmpScore_rank',
+                                          'EduScore_rank', 'HDDScore_rank', 'CriScore_rank',
+                                          'BHSScore_rank', 'EnvScore_rank', 'area_rank',
+                                          'Total_rank')))
+
+x_start <- 2
+x_span <- 2
+metric_names <- c('3', '30', '300', 'Water \nDistance', 'NDVI', 'NDWI', 'NDBI', 'IMD')
+text_labels_df <- tibble(metric_names, y = 56.3,
+                         x = seq(x_start, x_start + x_span * (nrow(text_labels_df) - 1), x_span))
+
+t3_30_300_rank_rgn_gdf <- t3_30_300_rank_gdf |> 
+    left_join(st_geometry(rgn22_gdf) |> 
+                  st_point_on_surface() |> 
+                  st_coordinates() |>
+                  as_tibble() |> bind_cols(rgn22_gdf |> select(RGN22CD, RGN22NM)), by = 'RGN22CD') |> 
+    mutate_at(vars(ends_with('rank')), ~ normalize(., range = c(st_bbox(rgn22_gdf)$ymin, st_bbox(rgn22_gdf)$ymax),
+                                                   method = 'range'))
+
+t3_30_300_spectral_rank_map <- t3_30_300_rank_rgn_gdf |> 
+    ggplot() +
+    geom_sf(data = rgn22_gdf, fill = 'transparent') +
+    geom_point(aes(x = X, y = Y, color = IMD)) +
+    
+    geom_sigmoid(linetype = 'dotted', aes(x = X, y = Y, xend = x_start + x_span * 0, yend = tree_count_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 0, y = tree_count_rank, color = IMD)) +
+    
+    geom_sigmoid(aes(x = x_start + x_span * 0, y = tree_count_rank, xend = x_start + x_span * 1, yend = canopy_cover_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 1, y = canopy_cover_rank, color = IMD)) +
+    
+    geom_sigmoid(aes(x = x_start + x_span * 1, y = canopy_cover_rank, xend = x_start + x_span * 2, yend = park_distance_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 2, y = park_distance_rank, color = IMD)) +
+    
+    geom_sigmoid(aes(x = x_start + x_span * 2, y = park_distance_rank, xend = x_start + x_span * 3, yend = water_distance_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 3, y = water_distance_rank, color = IMD)) +
+    
+    geom_sigmoid(aes(x = x_start + x_span * 3, y = water_distance_rank, xend = x_start + x_span * 4, yend = NDVI_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 4, y = NDVI_rank, color = IMD)) +
+    
+    geom_sigmoid(aes(x = x_start + x_span * 4, y = NDVI_rank, xend = x_start + x_span * 5, yend = NDWI_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 5, y = NDWI_rank, color = IMD)) +
+    
+    geom_sigmoid(aes(x = x_start + x_span * 5, y = NDWI_rank, xend = x_start + x_span * 6, yend = NDBI_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 6, y = NDBI_rank, color = IMD)) +
+    
+    geom_sigmoid(aes(x = x_start + x_span * 6, y = NDBI_rank, xend = x_start + x_span * 7, yend = IMDScore_rank, group = RGN22CD, color = IMD)) +
+    geom_point(aes(x = x_start + x_span * 7, y = IMDScore_rank, color = IMD)) +
+    
+    geom_text(aes(x = x_start + x_span * 7 + .5, y = IMDScore_rank, 
+                  label = str_wrap(RGN22NM, width = 15)), size = 2, hjust = 0) +
+    
+    geom_text(data = text_labels_df, aes(x = x, y = y, label = metric_names), size = 2) +
+    scale_x_continuous(limits = c(-5.5, 18)) +
+    scale_color_brewer(palette = "RdYlBu") +
+    guides(color = guide_legend(nrow = 1)) +
+    theme_void() +
+    theme(plot.background = element_rect(fill = "white", color = 'transparent'),
+          legend.position = "none",
+          legend.title = element_text(size = 20),
+          legend.text = element_text(size = 15))
+
+ggsave("images/t3_30_300_spectral_rank_map.png", t3_30_300_spectral_rank_map, 
+       width = 180, height = 90, units = 'mm', dpi = 300)
+
+
+t3_30_300_rank_long_gdf |> 
+    filter(Metric %in% c('tree_count_rank', 'canopy_cover_rank',
+                         'park_distance_rank', 'water_distance_rank', 
+                         'NDVI_rank', 'NDBI_rank', 'NDWI_rank', 'IMDScore_rank')) |> 
+    left_join(t3_30_300_gdf |> 
+                  st_drop_geometry() |> 
+                  select(RGN22CD, RGN22NM), by = 'RGN22CD') |> 
+    ggplot(aes(x = Metric, y = Rank, group = RGN22CD, colour = IMD)) + 
+    geom_point(size = 3) +
+    geom_bump(size = 2, smooth = 8) +
+    # geom_text(aes(label = RGN22NM)) +
+    # scale_y_reverse() +
+    scale_y_continuous(breaks = 1:10) +
+    scale_color_brewer(palette = 'RdYlBu') +
+    labs(x = 'Metric', y = 'Rank') +
+    plot_theme
+
+
+# Low Scors -> High Ranking -> Low Deprivation
+# High Score -> Low Ranking -> High Deprivation
+
 # Scatter Plots -----------------------------------------------------------
 
 t3_30_300_gdf |> 
@@ -191,125 +315,3 @@ t3_30_300_cor |>
     corrplot(method = "circle", type = "upper",# order = "hclust",
              tl.col = "black", tl.srt = 45)
 
-# Rank Map ----------------------------------------------------------------
-
-lsoa_to_bua_df <- read_csv(lsoa_to_bua_path)
-lad_to_bua_gdf <- read_sf(lad_to_bua_path)
-
-rgn22_gdf <- lad_to_bua_gdf |> 
-    left_join(lsoa_to_bua_df |> 
-                  select(LAD22CD, RGN22CD, RGN22NM), by = c('LAD22CD' = 'LAD22CD')) |>
-    select(RGN22CD, RGN22NM, geometry) |> 
-    group_by(RGN22CD, RGN22NM) |> 
-    summarise() |> 
-    mutate(RGN22NM = if_else(RGN22NM == 'Wales', NA, RGN22NM)) |>
-    mutate(RGN22NM = fct_relevel(RGN22NM, c('North West', 'North East',
-                                            'Yorkshire and The Humber',
-                                            'West Midlands', 'East Midlands',
-                                            'East of England', 'South West',
-                                            'South East', 'London', NA))) |> 
-    filter(!is.na(RGN22NM))
-
-t3_30_300_rank_gdf <- t3_30_300_gdf |> 
-    st_drop_geometry() |> 
-    select(RGN22CD, ends_with('Score'), park_distance, water_distance,
-           tree_count, canopy_cover, NDVI, NDBI, NDWI, area, Total) |> 
-    filter(!is.na(RGN22CD), RGN22CD != 'W92000004') |> 
-    group_by(RGN22CD) |> 
-    summarise(across(ends_with('Score'), mean, .names = '{.col}'),
-              across(area:Total, sum, .names = '{.col}'),
-              across(park_distance:NDWI, ~ mean(.x, na.rm = TRUE), .names = '{.col}')) |>
-    mutate(across(IMDScore:water_distance, function(x) {rank(-x)}, .names = '{.col}_rank')) |> 
-    mutate(across(tree_count:NDWI, function(x) {rank(x)}, .names = '{.col}_rank')) |> 
-    mutate(IMD = as.character(IMDScore_rank)) |> 
-    select(RGN22CD, IMD, ends_with('rank'))
-
-t3_30_300_rank_long_gdf <- t3_30_300_rank_gdf |> 
-    pivot_longer(ends_with('rank'), names_to = 'Metric', values_to = 'Rank') |> 
-    mutate(Metric = fct_relevel(Metric, c('IMDScore_rank', 'IncScore_rank', 'EmpScore_rank',
-                                          'EduScore_rank', 'HDDScore_rank', 'CriScore_rank',
-                                          'BHSScore_rank', 'EnvScore_rank', 'area_rank',
-                                          'Total_rank', 'tree_count_rank', 'park_distance_rank',
-                                          'canopy_cover_rank', 'water_distance_rank',
-                                          'NDVI_rank', 'NDBI_rank', 'NDWI_rank')))
-
-x_start <- 2
-x_span <- 2
-metric_names <- c('3', '30', '300', 'Water \nDistance', 'NDVI', 'NDWI', 'NDBI', 'IMD')
-text_labels_df <- tibble(metric_names, y = 56.3,
-                         x = seq(x_start, x_start + x_span * (nrow(text_labels_df) - 1), x_span))
-
-t3_30_300_rank_rgn_gdf <- t3_30_300_rank_gdf |> 
-    left_join(st_geometry(rgn22_gdf) |> 
-                  st_point_on_surface() |> 
-                  st_coordinates() |>
-                  as_tibble() |> bind_cols(rgn22_gdf |> select(RGN22CD, RGN22NM)), by = 'RGN22CD') |> 
-    mutate_at(vars(ends_with('rank')), ~ normalize(., range = c(st_bbox(rgn22_gdf)$ymin, st_bbox(rgn22_gdf)$ymax),
-                                                   method = 'range'))
-
-t3_30_300_spectral_rank_map <- t3_30_300_rank_rgn_gdf |> 
-    ggplot() +
-    geom_sf(data = rgn22_gdf, fill = 'transparent') +
-    geom_point(aes(x = X, y = Y, color = IMD)) +
-    
-    geom_sigmoid(aes(x = X, y = Y, xend = x_start + x_span * 0, yend = tree_count_rank, group = RGN22CD, color = IMD)) +
-    geom_point(aes(x = x_start + x_span * 0, y = tree_count_rank, color = IMD)) +
-    
-    geom_sigmoid(aes(x = x_start + x_span * 0, y = tree_count_rank, xend = x_start + x_span * 1, yend = canopy_cover_rank, group = RGN22CD, color = IMD)) +
-    geom_point(aes(x = x_start + x_span * 1, y = canopy_cover_rank, color = IMD)) +
-    
-    geom_sigmoid(aes(x = x_start + x_span * 1, y = canopy_cover_rank, xend = x_start + x_span * 2, yend = park_distance_rank, group = RGN22CD, color = IMD)) +
-    geom_point(aes(x = x_start + x_span * 2, y = park_distance_rank, color = IMD)) +
-    
-    geom_sigmoid(aes(x = x_start + x_span * 2, y = park_distance_rank, xend = x_start + x_span * 3, yend = water_distance_rank, group = RGN22CD, color = IMD)) +
-    geom_point(aes(x = x_start + x_span * 3, y = water_distance_rank, color = IMD)) +
-    
-    geom_sigmoid(aes(x = x_start + x_span * 3, y = water_distance_rank, xend = x_start + x_span * 4, yend = NDVI_rank, group = RGN22CD, color = IMD)) +
-    geom_point(aes(x = x_start + x_span * 4, y = NDVI_rank, color = IMD)) +
-    
-    geom_sigmoid(aes(x = x_start + x_span * 4, y = NDVI_rank, xend = x_start + x_span * 5, yend = NDWI_rank, group = RGN22CD, color = IMD)) +
-    geom_point(aes(x = x_start + x_span * 5, y = NDWI_rank, color = IMD)) +
-    
-    geom_sigmoid(aes(x = x_start + x_span * 5, y = NDWI_rank, xend = x_start + x_span * 6, yend = NDBI_rank, group = RGN22CD, color = IMD)) +
-    geom_point(aes(x = x_start + x_span * 6, y = NDBI_rank, color = IMD)) +
-    
-    geom_sigmoid(aes(x = x_start + x_span * 6, y = NDBI_rank, xend = x_start + x_span * 7, yend = IMDScore_rank, group = RGN22CD, color = IMD)) +
-    geom_point(aes(x = x_start + x_span * 7, y = IMDScore_rank, color = IMD)) +
-    
-    geom_text(aes(x = x_start + x_span * 7 + .5, y = IMDScore_rank, 
-                  label = str_wrap(RGN22NM, width = 15)), size = 2, hjust = 0) +
-    
-    geom_text(data = text_labels_df, aes(x = x, y = y, label = metric_names), size = 2) +
-    scale_x_continuous(limits = c(-5.5, 18)) +
-    scale_color_brewer(palette = "RdYlBu") +
-    guides(color = guide_legend(nrow = 1)) +
-    theme_void() +
-    theme(plot.background = element_rect(fill = "white", color = 'transparent'),
-          legend.position = "none",
-          legend.title = element_text(size = 20),
-          legend.text = element_text(size = 15))
-
-ggsave("images/t3_30_300_spectral_rank_map.png", t3_30_300_spectral_rank_map, 
-       width = 180, height = 90, units = 'mm', dpi = 300)
-
-
-t3_30_300_rank_long_gdf |> 
-    filter(Metric %in% c('tree_count_rank', 'canopy_cover_rank',
-                         'park_distance_rank', 'water_distance_rank', 
-                         'NDVI_rank', 'NDBI_rank', 'NDWI_rank', 'IMDScore_rank')) |> 
-    left_join(t3_30_300_gdf |> 
-                  st_drop_geometry() |> 
-                  select(RGN22CD, RGN22NM), by = 'RGN22CD') |> 
-ggplot(aes(x = Metric, y = Rank, group = RGN22CD, colour = IMD)) + 
-    geom_point(size = 3) +
-    geom_bump(size = 2, smooth = 8) +
-    # geom_text(aes(label = RGN22NM)) +
-    # scale_y_reverse() +
-    scale_y_continuous(breaks = 1:10) +
-    scale_color_brewer(palette = 'RdYlBu') +
-    labs(x = 'Metric', y = 'Rank') +
-    plot_theme
-
-
-# Low Scors -> High Ranking -> Low Deprivation
-# High Score -> Low Ranking -> High Deprivation
