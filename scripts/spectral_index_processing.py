d06d815604165ea20208b419df2503a96abb7c7a
Andrés Camilo Zúñiga-González <ancazugo@hotmail.com>
2025-03-01 12:59:12 +0000

    Process max for a date range

--- scripts/spectral_index_processing.py
+++ scripts/spectral_index_processing.py
@@ -9,7 +9,7 @@ import ee, eemont
 import geopandas as gpd
 from shapely.geometry import shape
 
-# Function to calculate mean NDVI for each feature
+# Function to calculate the median for each feature (out of the max values for a given date range)
 def calculate_median_index(image_collection: ee.image.Image, geometries: ee.featurecollection.FeatureCollection, 
                            scale: float=10.0, tile_scale: int=4) -> ee.featurecollection.FeatureCollection:
     """
@@ -23,11 +23,11 @@ def calculate_median_index(image_collection: ee.image.Image, geometries: ee.feat
         ee.featurecollection.FeatureCollection: A FeatureCollection containing the median index values for each geometry.
     """
 
-    mean_dict = image_collection.reduceRegions(
+    median_dict = image_collection.reduceRegions(
         collection=geometries,
         reducer=ee.Reducer.median(), scale=scale, tileScale=tile_scale
     )
-    return mean_dict
+    return median_dict
 
 def calculate_zonal_statistics(imd_lsoa_bua_boundaries_ee: ee.featurecollection.FeatureCollection, iter_span: int=100) -> list:
     """
@@ -61,7 +61,7 @@ def calculate_zonal_statistics(imd_lsoa_bua_boundaries_ee: ee.featurecollection.
                 time.sleep(5)  # Wait before retrying
 
     end_time = time.time()
-    logging.info(f"Calculating spectral zonal statistics took {end_time - start_time:.2f} seconds")
+    logging.warning(f"Calculating spectral zonal statistics took {end_time - start_time:.2f} seconds")
 
     return spectral_results_lst
 
@@ -88,17 +88,17 @@ if __name__ == "__main__":
 
     T3_30_300_DIR = VECTOR_OUT_DIR / "3-30-300"
     imd_lsoa_bua_boundaries_ee_path = "projects/ee-phd-thesis/assets/English_IMD_2019_BUA_filtered_boundaries"
-    spectral_indexes_path = T3_30_300_DIR / "spectral_indexes.geojson"
+    spectral_indexes_path = T3_30_300_DIR / f"spectral_indexes_{start_date}_{end_date}.geojson"
 
     log_path = Path("logs/spectral_indexes_calculation.log")
     setup_logger(log_path=log_path, log_level=log_level)
-    logging.info("Calculating spectral indexes for all geographies")
+    logging.warning(f"Calculating spectral indexes for all geographies between {start_date} and {end_date}")
 
-    logging.debug("Initializing GEE")
+    logging.warning("Initializing GEE")
     ee.Authenticate()
     ee.Initialize(project=GEE_PROJECT_NAME, opt_url='https://earthengine-highvolume.googleapis.com')
 
-    logging.debug("Querying GEE for spectral indexes")
+    logging.warning("Querying GEE for spectral indexes")
     imd_lsoa_bua_boundaries_ee = ee.FeatureCollection(imd_lsoa_bua_boundaries_ee_path)
     england_boundaries = ee.FeatureCollection("FAO/GAUL/2015/level1") \
         .filter(ee.Filter.eq('ADM1_NAME', 'England'))
@@ -109,7 +109,7 @@ if __name__ == "__main__":
         .filterDate(start_date, end_date) \
         .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', cloud_coverage)) \
         .filterBounds(england_geometry) \
-        .spectralIndices(indexes).median() \
+        .spectralIndices(indexes).max() \
         .select(indexes))
 
     spectral_results_lst = calculate_zonal_statistics(imd_lsoa_bua_boundaries_ee)    
