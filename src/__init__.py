8fffef2d16518295b8c1acaea6ef9229efdc5927
Andrés Camilo Zúñiga-González <ancazugo@hotmail.com>
2025-03-27 14:36:44 +0000

    Initial Python scripts

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# src/__init__.py
+from utils.constants import *
+from utils.install_jdk import *
+from utils.logging_config import *
+from .t3 import *
+from .t30 import *
+from .t300 import *
+
---
+++ src/main.py
@@ -0,0 +1,10 @@
+# src/main.py
+from t3 import *
+from t30 import *
+from t300 import *
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
