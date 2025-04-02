30cd15328cc938950169d8c7be7186890493c294
Andrés Camilo Zúñiga-González <ancazugo@hotmail.com>
2025-04-02 11:09:42 +0100

    code: first commit on main modules

--- src/t3.py
+++ src/t3.py
@@ -4,3 +4,200 @@ Description: Functions for cleaning and transforming data in My Project.
 Author: Your Name
 Date: YYYY-MM-DD
 """
+
+from src.utils.paths import *
+from src.utils.constants import *
+from src.utils.logging_config import *
+from src.utils.data_processing import translate_tile_name, get_overlapping_grid_tiles
+from src.utils.vom_processing import extract_grid_reference
+
+from sedona.utils.adapter import Adapter
+from sedona.core.enums import GridType
+from sedona.core.enums import IndexType
+from sedona.core.spatialOperator import JoinQueryRaw
+
+import time
+import pandas as pd
+import geopandas as gpd
+
+def check_tree_vom_pair(chm_path: str|Path, trees_dir: str|Path) -> bool:
+    """
+    Check if a CHM file has a corresponding tree file.
+    Parameters:
+        chm_path (str | Path): The path to the CHM file.
+        trees_dir (str | Path): The directory containing the tree files.
+    Returns:
+        bool: True if a corresponding tree file exists, otherwise False.
+    """
+
+    logging.debug(f"Checking if {chm_path} has a corresponding tree file in {trees_dir}")
+
+    tile_name = extract_grid_reference(chm_path)
+    chm_path = chm_path if isinstance(chm_path, Path) else Path(chm_path)
+    trees_dir = trees_dir if isinstance(trees_dir, Path) else Path(trees_dir)
+    year = chm_path.parent.name
+
+    trees_path = trees_dir / f"VOM_trees_{tile_name}_{year}.gpkg"
+
+    if trees_path.exists():
+        return str(trees_path)
+    
+def process_vom_tiles(sedona, trees_path_lst: list, tree_area: int=10, tree_height: int=3) -> gpd.GeoDataFrame:
+
+    logging.debug(f"Reading {len(trees_path_lst)} VOM tiles")
+
+    if len(trees_path_lst) > 1:
+        
+        trees_gdf_lst = [gpd.read_file(tree_path) for tree_path in trees_path_lst]
+        merged_trees_gdf = gpd.GeoDataFrame(pd.concat(trees_gdf_lst, ignore_index=True))
+    elif len(trees_path_lst) == 1: 
+        merged_trees_gdf = gpd.read_file(trees_path_lst[0])
+
+    else:
+        return None
+
+    geo_trees_gdf = merged_trees_gdf[(merged_trees_gdf.area > tree_area) & (merged_trees_gdf.height > tree_height)].reset_index(drop=True)
+    geo_trees_gdf['treeID'] = range(len(geo_trees_gdf))
+
+    geo_trees_gdf['geometry'] = geo_trees_gdf['geometry'].centroid
+
+    geo_trees_sdf = sedona.createDataFrame(geo_trees_gdf)
+    geo_trees_sdf.createOrReplaceTempView("geo_trees")
+
+    return geo_trees_gdf
+
+def process_buildings(sedona, geo_level: str, geo_code: str, buffer: int=100) -> None:
+
+    logging.debug(f"Filtering buildings for {geo_code}")
+
+    geo_boundary_sdf = sedona.sql(
+        f"""
+            SELECT ST_Union_Aggr(geometry) AS geometry
+            FROM boundaries
+            WHERE {geo_level} = '{geo_code}'
+        """)
+    geo_boundary_sdf.createOrReplaceTempView("geo_boundary")
+
+    geo_buildings_sdf = sedona.sql(
+        """
+            SELECT b.* FROM buildings b, geo_boundary g 
+            WHERE ST_Intersects(b.geometry, g.geometry)
+        """)
+    geo_buildings_sdf.createOrReplaceTempView("geo_buildings")
+
+    geo_buildings_buffer_sdf = sedona.sql(
+        f"""
+            SELECT ST_Buffer(b.geometry, {buffer}) AS geometry, b.verisk_premise_id
+            FROM geo_buildings b
+        """)
+    geo_buildings_buffer_sdf.createOrReplaceTempView("building_buffers")
+
+    return geo_buildings_buffer_sdf
+
+def count_trees(sedona, geo_code: str) -> pd.DataFrame:
+
+    logging.debug(f"Counting trees for each building in {geo_code}")
+
+    trees_within_buffer_sdf = sedona.sql(
+        """
+            SELECT b.verisk_premise_id, COUNT(t.treeID) AS tree_count
+            FROM building_buffers b
+            LEFT JOIN geo_trees t
+            ON ST_Intersects(b.geometry, t.geometry)
+            GROUP BY b.verisk_premise_id
+        """)
+    
+    trees_within_buffer_df = trees_within_buffer_sdf.toPandas()
+
+    return trees_within_buffer_df
+
+def get_vom_trees_paths(overlapping_tiles_lst: list, vom_tree_pair_dict: dict) -> list:
+
+    logging.debug(f"Getting VOM trees paths for {len(overlapping_tiles_lst)} overlapping tiles")
+
+    vom_trees_path_lst = list(set([path_pair[1] for _,v in vom_tree_pair_dict.items() for path_pair in v if path_pair[1] is not None]))
+    trees_path_lst = []
+    for tile_name in overlapping_tiles_lst:
+        translated_tile_name = translate_tile_name(tile_name).upper()
+        tile_path = [path for path in vom_trees_path_lst if translated_tile_name in path]
+        trees_path_lst.append(tile_path[0])
+
+    return trees_path_lst
+
+def create_spatial_rdds(geo_buildings_buffer_sdf, geo_trees_sdf):
+
+    logging.debug(f"Creating Spatial RDDs for buildings and trees")
+
+    geo_buildings_buffer_rdd  = Adapter.toSpatialRdd(geo_buildings_buffer_sdf, 'geometry')
+    geo_trees_rdd = Adapter.toSpatialRdd(geo_trees_sdf, 'geometry')
+    
+    geo_buildings_buffer_rdd.analyze()
+    geo_trees_rdd.analyze()
+
+    return geo_buildings_buffer_rdd, geo_trees_rdd
+
+def count_trees_rdd(sedona, geo_buildings_buffer_rdd, geo_trees_rdd, build_on_spatial_partitioned_rdd = True, using_index = True):
+
+    logging.debug(f"Counting trees for each building using RDD")
+
+    geo_trees_rdd.spatialPartitioning(GridType.KDBTREE)
+    geo_buildings_buffer_rdd.spatialPartitioning(geo_trees_rdd.getPartitioner())
+    
+    geo_buildings_buffer_rdd.buildIndex(IndexType.QUADTREE, build_on_spatial_partitioned_rdd)
+
+    query_result = JoinQueryRaw.SpatialJoinQueryFlat(geo_trees_rdd, geo_buildings_buffer_rdd, using_index, True)
+
+    query_result_sdf = Adapter.toDf(query_result, ["verisk_premise_id"], ["treeID"], sedona)
+
+    query_result_df = query_result_sdf.toPandas().sort_values(by='verisk_premise_id')
+
+    trees_within_buffer_df = query_result_df.groupby('verisk_premise_id').size().reset_index(name='tree_count')
+
+    return trees_within_buffer_df
+
+def read_vom_trees_geoparquet(sedona, overlapping_tiles_lst):
+
+    vom_trees_dir = T3_30_300_DIR / "VOM_Trees_geoparquet"
+    vom_trees_paths = [str(path) for path in vom_trees_dir.glob("*.geoparquet") if any(tile_name in path.name for tile_name in overlapping_tiles_lst)]
+    geo_trees_sdf = sedona.read.format("geoparquet").load(vom_trees_paths)
+    geo_trees_sdf.createOrReplaceTempView("geo_trees")
+
+def process_geo_code(sedona, query_method: str, geo_level: str, geo_code: str, tile_level: str, vom_tree_pair_dict: dict,
+                     output_areas_boundaries_gdf: gpd.GeoDataFrame, os_tile_boundaries_gdf: gpd.GeoDataFrame,
+                     tree_area: int=10, tree_height: int=3, buffer: int=100) -> None:
+
+    start_time = time.time()
+
+    logging.info(f"Processing {geo_code} with buffer {buffer}m")
+
+    tree_count_path = T3_dir / f"T3_{geo_code}_{buffer}m.csv"
+    if not tree_count_path.exists():
+        try:
+            geo_buildings_buffer_sdf = process_buildings(sedona, geo_level, geo_code, buffer)
+            overlapping_tiles_lst = get_overlapping_grid_tiles(output_areas_boundaries_gdf, os_tile_boundaries_gdf, geo_level, geo_code, tile_level)
+            if query_method == 'sql':
+
+                logging.debug(f"Executing query using SQL")
+
+                trees_path_lst = get_vom_trees_paths(overlapping_tiles_lst, vom_tree_pair_dict)
+                geo_trees_gdf = process_vom_tiles(sedona, trees_path_lst, tree_area=tree_area, tree_height=tree_height)
+
+                trees_within_buffer_df = count_trees(sedona, geo_level, geo_code)
+            
+            elif query_method == 'rdd':
+
+                logging.debug(f"Executing query using Spatial RDD")
+                
+                geo_trees_sdf = read_vom_trees_geoparquet(sedona, overlapping_tiles_lst)
+                geo_buildings_buffer_rdd, geo_trees_rdd = create_spatial_rdds(geo_buildings_buffer_sdf, geo_trees_sdf)
+                trees_within_buffer_df = count_trees_rdd(sedona, geo_buildings_buffer_rdd, geo_trees_rdd, build_on_spatial_partitioned_rdd = True, using_index = True)
+
+            trees_within_buffer_df.to_csv(tree_count_path, index=False)
+            
+            logging.info(f"Saving file for {geo_code} with {len(trees_within_buffer_df)} records")
+
+            end_time = time.time()
+            logging.info(f"Processing for {geo_code} took {end_time - start_time:.2f} seconds")
+
+        except Exception as e:
+            logging.error(f"Error processing {geo_code}: {e}")
--- src/t30.py
+++ src/t30.py
@@ -4,3 +4,162 @@ Description: Functions for cleaning and transforming data in My Project.
 Author: Your Name
 Date: YYYY-MM-DD
 """
+
+from src.utils.paths import *
+from src.utils.constants import *
+from src.utils.logging_config import *
+from src.utils.data_processing import translate_tile_name
+
+import time, logging
+import pandas as pd
+import geopandas as gpd
+import xarray as xr
+import rioxarray as rxr
+from rioxarray.merge import merge_arrays
+from rasterstats import zonal_stats
+
+def generate_tiles_paths(geo_level: str, geo_code: str, output_areas_os_tile_overlay_df: gpd.GeoDataFrame, vom_raster_paths_df) -> tuple:
+
+    logging.debug(f"Generating tile paths for {geo_code}")
+
+    geo_output_areas_os_tile_overlay_df = output_areas_os_tile_overlay_df[output_areas_os_tile_overlay_df[geo_level] == geo_code]
+    vom_raster_paths_df['TILE_NAME_5KM_int'] = vom_raster_paths_df.TILE_NAME.apply(lambda x: x.lower())
+    geo_vom_tiles_df = geo_output_areas_os_tile_overlay_df.merge(vom_raster_paths_df, on='TILE_NAME_5KM_int', how='left')[['TILE_NAME', 'year', 'path']].drop_duplicates().sort_values(['TILE_NAME', 'year'], ascending=[True, False]).reset_index(drop=True)
+
+    geo_selected_vom_tiles_df = geo_vom_tiles_df.groupby(['TILE_NAME']).first().reset_index()
+
+    return geo_vom_tiles_df, geo_selected_vom_tiles_df
+
+def select_chm_files(geo_vom_tiles_df: pd.DataFrame, geo_selected_vom_tiles_df: pd.DataFrame) -> list:
+    """
+    Selects CHM (Canopy Height Model) files based on the provided GeoDataFrames and DataFrame of file paths.
+    Parameters:
+        geo_selected_vom_tiles_df (pd.DataFrame): DataFrame containing selected VOM tiles with columns 'TILE_NAME' and 'year'.
+        geo_vom_tiles_df (pd.DataFrame): DataFrame containing VOM tiles with columns 'TILE_NAME' and 'year'.
+    Returns:
+        list: A list of Path objects pointing to the selected CHM files.
+    """
+
+    logging.debug("Selecting CHM files")
+
+    vom_raster_paths_df = pd.read_parquet(vom_raster_paths_parquet)
+
+    selected_chm_path_lst = []
+    for row in geo_selected_vom_tiles_df.itertuples():
+        tile_name = translate_tile_name(row.TILE_NAME).upper()
+        year = row.year
+        temp_df = vom_raster_paths_df[((vom_raster_paths_df['TILE_NAME'] == tile_name) & (vom_raster_paths_df['year'] == year)) & (vom_raster_paths_df['file_type'] == 'CHM')]
+        if len(temp_df) == 1:
+            selected_chm_path_lst.append(Path(temp_df.path.iloc[0]))
+        else:
+            tile_df = geo_vom_tiles_df[geo_vom_tiles_df['TILE_NAME'] == row.TILE_NAME]
+            tile_years_lst = tile_df.year.tolist()
+            tile_years_lst.remove(year)
+            ind = 0
+            while len(tile_years_lst) > 0 and ind < len(tile_years_lst):
+                temp_df = vom_raster_paths_df[((vom_raster_paths_df['TILE_NAME'] == tile_name) & (vom_raster_paths_df['year'] == tile_years_lst[ind])) & (vom_raster_paths_df['file_type'] == 'CHM')]
+                if len(temp_df) == 1:
+                    selected_chm_path_lst.append(Path(temp_df.path.iloc[0]))
+                    ind = len(tile_years_lst) + 1
+                else:
+                    ind += 1
+
+    return selected_chm_path_lst
+
+def binarise_tiles(selected_chm_path_lst, low_threshold, high_threshold) -> xr.DataArray:
+    """
+    Binarise the canopy height model (CHM) tiles based on given thresholds.
+
+    This function reads a list of CHM files, merges them into a single xarray DataArray,
+    and then binarises the merged array based on the provided low and high thresholds.
+    The resulting binary array will have values of 1 where the CHM values are within the
+    specified range, and 0 otherwise.
+    Parameters:
+        selected_chm_path_lst (list of str): List of file paths to the CHM tiles to be processed.
+        low_threshold (float): The lower threshold for binarisation.
+        high_threshold (float): The upper threshold for binarisation.
+    Returns:
+        xr.DataArray: A binary xarray DataArray where values are 1 if within the threshold range, and 0 otherwise.
+    """
+
+    logging.warning(f"Binarising {len(selected_chm_path_lst)} VOM tiles")
+
+    chm_xr_lst = []
+    for file in selected_chm_path_lst:
+        try:
+            temp_rast = rxr.open_rasterio(file)
+            temp_rast.values
+            chm_xr_lst.append(temp_rast)
+            
+        except Exception as e:
+            logging.error(f"Error reading file: {file} - {e}")
+    merged_chm_xr = merge_arrays(chm_xr_lst)
+
+    binary_merged_chm_xr = (merged_chm_xr >= low_threshold) & (merged_chm_xr <= high_threshold)
+    binary_merged_chm_xr = binary_merged_chm_xr.astype(int).fillna(0)
+
+    return binary_merged_chm_xr
+
+def get_canopy_cover(subgeo_filt_gdf: gpd.GeoDataFrame, binary_merged_chm_xr: xr.DataArray) -> pd.DataFrame:
+    """
+    Calculate the canopy cover percentage for each geometry in the given GeoDataFrame.
+    Parameters:
+        subgeo_filt_gdf (gpd.GeoDataFrame): A GeoDataFrame containing the geometries for which the canopy cover is to be calculated.
+        binary_merged_chm_xr (xr.DataArray): A DataArray containing binary canopy height model data.
+    Returns:
+        pd.DataFrame: A DataFrame containing the original geometries and their corresponding canopy cover percentages.
+    """
+
+    logging.warning("Calculating canopy cover")
+
+    zs_categorical = zonal_stats(subgeo_filt_gdf, binary_merged_chm_xr[0].values, 
+                                affine=binary_merged_chm_xr.rio.transform(), categorical=True)
+
+    subgeo_filt_gdf['canopy_cover'] = [round(100 * z.get(1, 0) / (z.get(0, 0) + z.get(1, 0)), 2) for z in zs_categorical]
+
+    # variables_to_keep = ['LSOA11CD', 'LSOA21CD', geo_level ,'canopy_cover']
+
+    # subgeo_canopy_cover_df = subgeo_filt_gdf.copy()[variables_to_keep]
+    subgeo_canopy_cover_df = subgeo_filt_gdf.copy()
+    
+    return subgeo_canopy_cover_df
+
+def process_geo_code(geo_level: str, geo_code: str, output_areas_boundaries_gdf: gpd.GeoDataFrame, 
+                     output_areas_os_tile_overlay_df, vom_raster_paths_df,
+                     low_threshold: int=3, high_threshold: int=60) -> None:
+    """
+    Processes geographical data for a given geographical code and level, and calculates the canopy cover.
+
+    Parameters:
+        geo_level (str): The geographical level (e.g., LSOA, BUA).
+        geo_code (str): The geographical code corresponding to the geo_level.
+        output_areas_boundaries_gdf (gpd.GeoDataFrame): A GeoDataFrame containing geographical data.
+        low_threshold (int, optional): The lower threshold for binarizing canopy height model tiles. Default is 3.
+        high_threshold (int, optional): The upper threshold for binarizing canopy height model tiles. Default is 60.
+
+    Returns:
+        None
+    """
+    start_time = time.time()
+    logging.info(f"Processing data for {geo_code}")
+
+    try:
+        canopy_cover_path = T30_dir / f"T30_{geo_code}.csv"
+        
+        subgeo_filt_gdf = output_areas_boundaries_gdf.copy()[output_areas_boundaries_gdf[geo_level].isin([geo_code])].reset_index(drop=True)
+        geo_vom_tiles_df, geo_selected_vom_tiles_df = generate_tiles_paths(geo_level, geo_code, output_areas_os_tile_overlay_df, vom_raster_paths_df)
+        selected_chm_path_lst = select_chm_files(geo_vom_tiles_df, geo_selected_vom_tiles_df)
+        binary_merged_chm_xr = binarise_tiles(selected_chm_path_lst, low_threshold, high_threshold)
+        subgeo_canopy_cover_df = get_canopy_cover(subgeo_filt_gdf, binary_merged_chm_xr)
+
+        subgeo_canopy_cover_df.to_csv(canopy_cover_path)
+
+        logging.info(f"Saving file for {geo_code} with {len(subgeo_canopy_cover_df)} records")
+
+        end_time = time.time()
+        logging.info(f"Processing for {geo_code} took {end_time - start_time:.2f} seconds")
+
+        return subgeo_canopy_cover_df
+
+    except Exception as e:
+        logging.error(f"Error processing {geo_code}: {e}")
