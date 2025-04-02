7a9d13d5a7c84522068d156e90ebffa334052e1f
Andrés Camilo Zúñiga-González <ancazugo@hotmail.com>
2025-04-02 18:44:05 +0100

    code: simplify functions

--- src/t30.py
+++ src/t30.py
@@ -8,7 +8,7 @@ Date: YYYY-MM-DD
 from src.utils.paths import *
 from src.utils.constants import *
 from src.utils.logging_config import *
-from src.utils.data_processing import translate_tile_name
+from src.utils.data_processing import generate_tile_paths
 
 import time, logging
 import pandas as pd
@@ -18,55 +18,44 @@ import rioxarray as rxr
 from rioxarray.merge import merge_arrays
 from rasterstats import zonal_stats
 
-def generate_tiles_paths(geo_level: str, geo_code: str, output_areas_os_tile_overlay_df: gpd.GeoDataFrame, vom_raster_paths_df) -> tuple:
+# def select_chm_files(geo_tiles_df: pd.DataFrame) -> list:
+#     """
+#     Selects CHM (Canopy Height Model) files based on the provided GeoDataFrames and DataFrame of file paths.
+#     Parameters:
+#         geo_tiles_df (pd.DataFrame): DataFrame containing VOM tiles with columns 'TILE_NAME' and 'year'.
+#     Returns:
+#         list: A list of Path objects pointing to the selected CHM files.
+#     """
 
-    logging.debug(f"Generating tile paths for {geo_code}")
+#     logging.debug("Selecting CHM files")
 
-    geo_output_areas_os_tile_overlay_df = output_areas_os_tile_overlay_df[output_areas_os_tile_overlay_df[geo_level] == geo_code]
-    vom_raster_paths_df['TILE_NAME_5KM_int'] = vom_raster_paths_df.TILE_NAME.apply(lambda x: x.lower())
-    geo_vom_tiles_df = geo_output_areas_os_tile_overlay_df.merge(vom_raster_paths_df, on='TILE_NAME_5KM_int', how='left')[['TILE_NAME', 'year', 'path']].drop_duplicates().sort_values(['TILE_NAME', 'year'], ascending=[True, False]).reset_index(drop=True)
+#     vom_raster_paths_df = pd.read_parquet(vom_raster_paths_parquet)
 
-    geo_selected_vom_tiles_df = geo_vom_tiles_df.groupby(['TILE_NAME']).first().reset_index()
+#     geo_selected_tiles_df = geo_tiles_df.groupby(['TILE_NAME']).first().reset_index()
 
-    return geo_vom_tiles_df, geo_selected_vom_tiles_df
+#     selected_chm_path_lst = []
+#     for row in geo_selected_tiles_df.itertuples():
+#         tile_name = translate_tile_name(row.TILE_NAME).upper()
+#         year = row.year
+#         temp_df = vom_raster_paths_df[(vom_raster_paths_df['TILE_NAME'] == tile_name) & (vom_raster_paths_df['year'] == year)]
+#         if len(temp_df) == 1:
+#             selected_chm_path_lst.append(Path(temp_df.path_vom.iloc[0]))
+#         else:
+#             tile_df = geo_tiles_df[geo_tiles_df['TILE_NAME'] == row.TILE_NAME]
+#             tile_years_lst = tile_df.year.tolist()
+#             tile_years_lst.remove(year)
+#             ind = 0
+#             while len(tile_years_lst) > 0 and ind < len(tile_years_lst):
+#                 temp_df = vom_raster_paths_df[(vom_raster_paths_df['TILE_NAME'] == tile_name) & (vom_raster_paths_df['year'] == tile_years_lst[ind])]
+#                 if len(temp_df) == 1:
+#                     selected_chm_path_lst.append(Path(temp_df.path_vom.iloc[0]))
+#                     ind = len(tile_years_lst) + 1
+#                 else:
+#                     ind += 1
 
-def select_chm_files(geo_vom_tiles_df: pd.DataFrame, geo_selected_vom_tiles_df: pd.DataFrame) -> list:
-    """
-    Selects CHM (Canopy Height Model) files based on the provided GeoDataFrames and DataFrame of file paths.
-    Parameters:
-        geo_selected_vom_tiles_df (pd.DataFrame): DataFrame containing selected VOM tiles with columns 'TILE_NAME' and 'year'.
-        geo_vom_tiles_df (pd.DataFrame): DataFrame containing VOM tiles with columns 'TILE_NAME' and 'year'.
-    Returns:
-        list: A list of Path objects pointing to the selected CHM files.
-    """
+#     return selected_chm_path_lst
 
-    logging.debug("Selecting CHM files")
-
-    vom_raster_paths_df = pd.read_parquet(vom_raster_paths_parquet)
-
-    selected_chm_path_lst = []
-    for row in geo_selected_vom_tiles_df.itertuples():
-        tile_name = translate_tile_name(row.TILE_NAME).upper()
-        year = row.year
-        temp_df = vom_raster_paths_df[((vom_raster_paths_df['TILE_NAME'] == tile_name) & (vom_raster_paths_df['year'] == year)) & (vom_raster_paths_df['file_type'] == 'CHM')]
-        if len(temp_df) == 1:
-            selected_chm_path_lst.append(Path(temp_df.path.iloc[0]))
-        else:
-            tile_df = geo_vom_tiles_df[geo_vom_tiles_df['TILE_NAME'] == row.TILE_NAME]
-            tile_years_lst = tile_df.year.tolist()
-            tile_years_lst.remove(year)
-            ind = 0
-            while len(tile_years_lst) > 0 and ind < len(tile_years_lst):
-                temp_df = vom_raster_paths_df[((vom_raster_paths_df['TILE_NAME'] == tile_name) & (vom_raster_paths_df['year'] == tile_years_lst[ind])) & (vom_raster_paths_df['file_type'] == 'CHM')]
-                if len(temp_df) == 1:
-                    selected_chm_path_lst.append(Path(temp_df.path.iloc[0]))
-                    ind = len(tile_years_lst) + 1
-                else:
-                    ind += 1
-
-    return selected_chm_path_lst
-
-def binarise_tiles(selected_chm_path_lst, low_threshold, high_threshold) -> xr.DataArray:
+def binarise_tiles(vom_paths_lst, low_threshold, high_threshold) -> xr.DataArray:
     """
     Binarise the canopy height model (CHM) tiles based on given thresholds.
 
@@ -82,10 +71,10 @@ def binarise_tiles(selected_chm_path_lst, low_threshold, high_threshold) -> xr.D
         xr.DataArray: A binary xarray DataArray where values are 1 if within the threshold range, and 0 otherwise.
     """
 
-    logging.warning(f"Binarising {len(selected_chm_path_lst)} VOM tiles")
+    logging.warning(f"Binarising {len(vom_paths_lst)} VOM tiles")
 
     chm_xr_lst = []
-    for file in selected_chm_path_lst:
+    for file in vom_paths_lst:
         try:
             temp_rast = rxr.open_rasterio(file)
             temp_rast.values
@@ -116,16 +105,14 @@ def get_canopy_cover(subgeo_filt_gdf: gpd.GeoDataFrame, binary_merged_chm_xr: xr
                                 affine=binary_merged_chm_xr.rio.transform(), categorical=True)
 
     subgeo_filt_gdf['canopy_cover'] = [round(100 * z.get(1, 0) / (z.get(0, 0) + z.get(1, 0)), 2) for z in zs_categorical]
+    subgeo_filt_gdf['total_pixels'] = [z.get(0, 0) + z.get(1, 0) for z in zs_categorical]
 
-    # variables_to_keep = ['LSOA11CD', 'LSOA21CD', geo_level ,'canopy_cover']
-
-    # subgeo_canopy_cover_df = subgeo_filt_gdf.copy()[variables_to_keep]
     subgeo_canopy_cover_df = subgeo_filt_gdf.copy()
     
     return subgeo_canopy_cover_df
 
 def process_geo_code(geo_level: str, geo_code: str, output_areas_boundaries_gdf: gpd.GeoDataFrame, 
-                     output_areas_os_tile_overlay_df, vom_raster_paths_df,
+                     output_areas_os_tile_overlay_df, vom_raster_paths_df, tree_vector_paths_df,
                      low_threshold: int=3, high_threshold: int=60) -> None:
     """
     Processes geographical data for a given geographical code and level, and calculates the canopy cover.
@@ -147,9 +134,10 @@ def process_geo_code(geo_level: str, geo_code: str, output_areas_boundaries_gdf:
         canopy_cover_path = T30_dir / f"T30_{geo_code}.csv"
         
         subgeo_filt_gdf = output_areas_boundaries_gdf.copy()[output_areas_boundaries_gdf[geo_level].isin([geo_code])].reset_index(drop=True)
-        geo_vom_tiles_df, geo_selected_vom_tiles_df = generate_tiles_paths(geo_level, geo_code, output_areas_os_tile_overlay_df, vom_raster_paths_df)
-        selected_chm_path_lst = select_chm_files(geo_vom_tiles_df, geo_selected_vom_tiles_df)
-        binary_merged_chm_xr = binarise_tiles(selected_chm_path_lst, low_threshold, high_threshold)
+        geo_tiles_df = generate_tile_paths(geo_level, geo_code, output_areas_os_tile_overlay_df, vom_raster_paths_df, tree_vector_paths_df)
+        # selected_chm_path_lst = select_chm_files(geo_tiles_df)
+        vom_paths_lst = geo_tiles_df.groupby('TILE_NAME').first().reset_index()['path_vom'].tolist()
+        binary_merged_chm_xr = binarise_tiles(vom_paths_lst, low_threshold, high_threshold)
         subgeo_canopy_cover_df = get_canopy_cover(subgeo_filt_gdf, binary_merged_chm_xr)
 
         subgeo_canopy_cover_df.to_csv(canopy_cover_path)
