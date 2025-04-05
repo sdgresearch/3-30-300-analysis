4f3ac8c59c9171c1ddf8884d26e19c61841129f7
Andrés Camilo Zúñiga-González <ancazugo@hotmail.com>
2025-04-05 12:15:55 +0100

    code: Rearrange imports

--- src/__init__.py
+++ src/__init__.py
@@ -2,13 +2,3 @@
 Package: src
 Description: This package contains the core modules for My Project.
 """
-from utils.constants import * 
-from utils.paths import *
-from utils.install_jdk import *
-from utils.logging_config import *
-from utils.data_processing import *
-from utils.vom_processing import *
-from .t3 import *
-from .t30 import *
-from .t300 import *
-
