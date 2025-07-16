a7b90873084408be7445fab644ef066b2e8e0319
Andrés Camilo Zúñiga-González <ancazugo@hotmail.com>
2025-07-16 16:42:24 +0100

    code: Format script for reorganising vom trees

---
+++ src/vom_trees_helper.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from utils.constants import PROJECT_CRS
+import shutil
+import os
+
+from tables_setup import *
+from utils.sedona_config import get_spark
+
+from pyspark.sql.functions import monotonically_increasing_id
+
+if __name__ == '__main__':
+    
+    sedona = get_spark()
+
+    tables = load_tables(sedona)
+    tree_vector_paths_df = tables['tree_vector_paths_df']
+    # Create target directory if it doesn't exist
+    target_dir = "/maps/acz25/phd-thesis-data/output/3-30-300/VOM_Trees_unique"
+    os.makedirs(target_dir, exist_ok=True)
+    unique_tree_paths = tree_vector_paths_df.drop_duplicates(subset=['TILE_NAME'])['path'].tolist()
+    # Copy each uniquefile to the target directory
+    for path in unique_tree_paths:
+        filename = os.path.basename(path)
+        target_path = os.path.join(target_dir, filename)
+        shutil.copy2(path, target_path)
+    # Save files as geoparquet in web mercator projection and centroided
+    trees_sdf = sedona.read.format("geopackage").option("tableName", "trees").load('/maps/acz25/phd-thesis-data/output/3-30-300/VOM_Trees_unique')
+    trees_sdf.createOrReplaceTempView("trees")
+
+    trees_sdf = sedona.sql(f"""SELECT treeID, height, area, ST_Transform(ST_Centroid(geom), "{PROJECT_CRS}", "EPSG:4326") AS geometry FROM trees""")
+    trees_sdf =  trees_sdf.withColumn("treeID", monotonically_increasing_id())
+
+    trees_sdf.write.format("geoparquet").save("/maps/acz25/phd-thesis-data/output/3-30-300/VOM_trees_parquet")
