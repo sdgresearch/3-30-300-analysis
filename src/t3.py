f1e30b72363b9524fd563a602dbe682a452ea283
Andrés Camilo Zúñiga-González <ancazugo@hotmail.com>
2025-07-18 10:50:22 +0100

    code: Correct duplicate tree processing

--- src/t3.py
+++ src/t3.py
@@ -5,15 +5,12 @@ Author: Andrés C. Zúñiga-González
 Date: 2025-07-16
 """
 
-from utils.paths import T3_30_300_DIR, T3_dir
-from utils.data_processing import generate_tile_paths, get_overlapping_grid_tiles, filter_buffer_geometries, get_geometries
+from utils.paths import T3_dir, trees_unique_dir
+from utils.data_processing import generate_tile_paths, get_overlapping_grid_tiles, filter_buffer_geometries, get_geometries, save_temp_file
 from utils.sedona_rdd import create_spatial_rdds, count_trees_rdd
 
 import time
-import glob
-import shutil
 import logging
-import tempfile
 import pandas as pd
 import geopandas as gpd
 from pyspark.sql.dataframe import DataFrame
@@ -56,21 +53,34 @@ def process_vom_tiles(sedona: SparkSession, trees_path_lst: list, tree_area: int
     return geo_trees_gdf
 
 # TODO: Figure out how to integrate the creation of the VOM_Trees geoparquet with the new version of the code
-def read_vom_trees_geoparquet(sedona: SparkSession, overlapping_tiles_lst: list) -> DataFrame:
+def read_vom_trees_unique(sedona: SparkSession, overlapping_tiles_lst: list, tree_area: int=10, tree_height: int=3) -> DataFrame:
     """
-    Reads the VOM trees geoparquet files.
+    Reads the VOM trees unique files.
     Args:
         sedona (SparkSession): The Spark session.
         overlapping_tiles_lst (list): The list of overlapping tiles.
+        tree_area (int): The area of the tree.
+        tree_height (int): The height of the tree.
     Returns:
         DataFrame: The VOM trees dataframe.
     """
 
-    vom_trees_dir = T3_30_300_DIR / "VOM_Trees_geoparquet"
-    vom_trees_paths = [str(path) for path in vom_trees_dir.glob("*.geoparquet") if any(tile_name in path.name for tile_name in overlapping_tiles_lst)]
-    geo_trees_sdf = sedona.read.format("geoparquet").load(vom_trees_paths)
+    logging.debug(f"Reading VOM trees vector files")
 
-    geo_trees_sdf.withColumn("treeID", monotonically_increasing_id()).createOrReplaceTempView("geo_trees")
+    vom_trees_paths = [str(path) for path in trees_unique_dir.glob("*.gpkg") if any(tile_name in path.name for tile_name in overlapping_tiles_lst)]
+
+    logging.debug(f"Found {len(vom_trees_paths)} VOM trees vector files")
+
+    # geo_trees_sdf = sedona.read.format("geopackage").option("tableName", "trees").load(str(temp_dir.name))
+    geo_trees_gdf = pd.concat([gpd.read_file(file) for file in vom_trees_paths], ignore_index=True)
+    geo_trees_sdf = sedona.createDataFrame(geo_trees_gdf)
+    if 'geom' in geo_trees_sdf.columns:
+        geo_trees_sdf = geo_trees_sdf.withColumnRenamed('geom', 'geometry')
+    geo_trees_sdf.createOrReplaceTempView("geo_trees")   
+    geo_trees_sdf = sedona.sql(f"""SELECT treeID, ST_Centroid(geometry) AS geometry FROM geo_trees 
+                               WHERE area > {tree_area} AND height > {tree_height}""")
+    geo_trees_sdf = geo_trees_sdf.withColumn("treeID", monotonically_increasing_id())
+    geo_trees_sdf.createOrReplaceTempView("geo_trees")
 
     return geo_trees_sdf
 
@@ -135,7 +145,8 @@ def process_geo_code(sedona: SparkSession, query_method: str, geo_level: str, ge
             get_geometries(sedona, geo_level, geo_code, True)
             geo_buildings_buffer_sdf = filter_buffer_geometries(sedona, geo_level, geo_code, 'buildings', buffer)
             overlapping_tiles_lst = get_overlapping_grid_tiles(output_areas_boundaries_gdf, os_tile_boundaries_gdf, geo_level, geo_code, tile_level)
-            
+            overlapping_tiles_lst = [tile_name.upper() for tile_name in overlapping_tiles_lst]
+
             if query_method == 'sql':
 
                 logging.debug("Executing query using SQL")
@@ -152,28 +163,11 @@ def process_geo_code(sedona: SparkSession, query_method: str, geo_level: str, ge
 
                 logging.debug("Executing query using Spatial RDD")
                 
-                geo_trees_sdf = read_vom_trees_geoparquet(sedona, overlapping_tiles_lst)
+                geo_trees_sdf = read_vom_trees_unique(sedona, overlapping_tiles_lst, tree_area=tree_area, tree_height=tree_height)
                 geo_buildings_buffer_rdd, geo_trees_rdd = create_spatial_rdds(geo_buildings_buffer_sdf, geo_trees_sdf, build_on_spatial_partitioned_rdd = True)
-                geo_tree_count_df = count_trees_rdd(sedona, geo_buildings_buffer_rdd, geo_trees_rdd, 'verisk_premise_id', using_index = True)
+                geo_tree_count_sdf = count_trees_rdd(sedona, geo_buildings_buffer_rdd, geo_trees_rdd, 'verisk_premise_id', using_index = True)
                 
-                temp_dir = tempfile.TemporaryDirectory()
-
-                geo_tree_count_df.coalesce(1) \
-                    .write \
-                    .option("header", True) \
-                    .mode("overwrite") \
-                    .csv(temp_dir.name)
-                
-                # Step 2: Find the part file Spark wrote
-                part_file = glob.glob(temp_dir.name + "/part-*.csv")[0]
-                
-                # Step 3: Move and rename it to your target file
-                shutil.move(part_file, str(geo_tree_count_path))
-
-                # Step 4: Clean up temp folder
-                temp_dir.cleanup()
-                
-                geo_tree_count_df = pd.read_csv(geo_tree_count_path)
+                geo_tree_count_df = save_temp_file(geo_tree_count_sdf, geo_tree_count_path)
                 geo_tree_count_df.rename(columns={'tree_count': f'tree_count_{buffer}m'}, inplace=True)
             
             end_time = time.time()
