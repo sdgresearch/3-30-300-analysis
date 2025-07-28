be82118fb985cdbe7f016531488062417a9496af
Andrés Camilo Zúñiga-González <ancazugo@hotmail.com>
2025-07-28 11:35:43 +0100

    code: Reorganise dataframe processing

--- R/utils/constants.R
+++ R/utils/constants.R
@@ -9,6 +9,8 @@ JAVA_HOME <- here(dirname(DATA_DIR), ".jdk", Sys.getenv("JDK_HOME"))
 Sys.setenv("JAVA_HOME"=JAVA_HOME)
 Sys.setenv("PROJ_LIB"=paste0(Sys.getenv("CONDA_PREFIX"), "/envs/r-env/share/proj"))
 PROJECT_CRS <- "EPSG:27700"  # OSGB 1936 / British National Grid
+WGS84_CRS <- "EPSG:4326"
+WEB_MERCATOR_CRS <- "EPSG:3857"
 
 if (!requireNamespace("apache.sedona", quietly = TRUE)) {
     install.packages("apache.sedona")
